63db56767d57bd4f54d21f4fa9479102c6902f4a
jjongwa <troas96@naver.com>
2023-07-06 23:35:54 +0900

    feat: BaseTimeEntity 구현
    엔티티의 생성/수정일자를 자동으로 넣어주기 위한 클래스

---
+++ backend/src/main/java/hanglog/global/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package hanglog.global;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public final class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
