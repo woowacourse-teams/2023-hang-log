7e561017ab494fa7359b6908824450724d992993
jjongwa <troas96@naver.com>
2023-07-13 02:33:40 +0900

    refactor: 생성 시점 기록 변경 방지 위한 기능 추가

--- backend/src/main/java/hanglog/global/BaseEntity.java
+++ backend/src/main/java/hanglog/global/BaseEntity.java
@@ -20,6 +20,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public class BaseEntity {
 
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
