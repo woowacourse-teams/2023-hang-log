81e924128998a4c445237f09538a8c6cbbb73bbd
jjongwa <troas96@naver.com>
2023-07-12 17:21:59 +0900

    refactor: Valid 조건 변경 및 테스트 추가
    Co-authored-by: hgo641 <hgo641@gmail.com>
    Co-authored-by: LJW25 <49433615+LJW25@users.noreply.github.com>

--- backend/src/main/java/hanglog/global/BaseEntity.java
+++ backend/src/main/java/hanglog/global/BaseEntity.java
@@ -1,5 +1,6 @@
 package hanglog.global;
 
+import static hanglog.global.type.StatusType.USABLE;
 import static jakarta.persistence.EnumType.STRING;
 
 import hanglog.global.type.StatusType;
@@ -7,10 +8,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
 import java.time.LocalDateTime;
 import lombok.Getter;
-import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -28,13 +27,5 @@ public class BaseEntity {
 
     @Column(nullable = false)
     @Enumerated(value = STRING)
-    @ColumnDefault("'USABLE'")
-    private StatusType status;
-
-    @PrePersist
-    public void prePersist() {
-        if (this.status == null) {
-            this.status = StatusType.USABLE;
-        }
-    }
+    private StatusType status = USABLE;
 }
--- backend/src/test/java/hanglog/trip/presentation/TripControllerTest.java
+++ backend/src/test/java/hanglog/trip/presentation/TripControllerTest.java
@@ -6,13 +6,16 @@ import static org.springframework.http.HttpHeaders.LOCATION;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hanglog.trip.presentation.dto.request.TripRequest;
 import hanglog.trip.service.TripService;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,5 +54,66 @@ class TripControllerTest {
                         .content(objectMapper.writeValueAsString(tripRequest)))
                 .andExpect(status().isCreated())
                 .andExpect(header().string(LOCATION, "/trips/1"));
+    }
+
+    @DisplayName("여행 시작 날짜를 입력하지 않으면 예외가 발생한다.")
+    @Test
+    void createTrip_StartDateNull() throws Exception {
+        // given
+        final TripRequest tripRequest = new TripRequest(null,
+                LocalDate.of(2023, 7, 7),
+                List.of(1L, 2L));
+
+        // when & then
+        mockMvc.perform(post("/trips")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(tripRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("여행 시작 날짜를 입력해주세요."));
+    }
+
+    @DisplayName("여행 종료 날짜를 입력하지 않으면 예외가 발생한다.")
+    @Test
+    void createTrip_EndDateNull() throws Exception {
+        // given
+        final TripRequest tripRequest = new TripRequest(LocalDate.of(2023, 7, 2),
+                null,
+                List.of(1L, 2L));
+
+        // when & then
+        mockMvc.perform(post("/trips")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(tripRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @DisplayName("입력받은 도시 리스트의 길이가 0이면 예외가 발생한다.")
+    @Test
+    void createTrip_CitesNull() throws Exception {
+        // given
+        final TripRequest tripRequest = new TripRequest(LocalDate.of(2023, 7, 2),
+                LocalDate.of(2023, 7, 7),
+                null);
+
+        // when & then
+        mockMvc.perform(post("/trips")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(tripRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @DisplayName("입력받은 도시 리스트의 길이가 0이면 예외가 발생한다.")
+    @Test
+    void createTrip_CitesEmpty() throws Exception {
+        // given
+        final TripRequest tripRequest = new TripRequest(LocalDate.of(2023, 7, 2),
+                LocalDate.of(2023, 7, 7),
+                Collections.EMPTY_LIST);
+
+        // when & then
+        mockMvc.perform(post("/trips")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(tripRequest)))
+                .andExpect(status().isBadRequest());
     }
 }
