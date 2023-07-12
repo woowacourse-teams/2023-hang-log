8d101355df45fcb6d737be3f919ae85597e4a409
LJW25 <dbjwlee@gmail.com>
2023-07-12 23:42:51 +0900

    chore: RestDocs 클래스 구현

---
+++ backend/src/test/java/hanglog/trip/restdocs/RestDocsConfiguration.java
@@ -0,0 +1,32 @@
+package hanglog.trip.restdocs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+import org.springframework.restdocs.snippet.Attributes.Attribute;
+
+
+@Configuration
+public class RestDocsConfiguration {
+
+    public static final Attribute field(final String key, final String value) {
+        return new Attribute(key, value);
+    }
+
+    @Bean
+    public RestDocumentationResultHandler write() {
+        return MockMvcRestDocumentation.document(
+                "{class-name}/{method-name}",
+                Preprocessors.preprocessRequest(
+                        Preprocessors.removeHeaders("Content-Length", "Host"),
+                        Preprocessors.prettyPrint()
+                ),
+                Preprocessors.preprocessResponse(
+                        Preprocessors.removeHeaders("Transfer-Encoding", "Date", "Keep-Alive", "Connection"),
+                        Preprocessors.prettyPrint()
+                )
+        );
+    }
+}
