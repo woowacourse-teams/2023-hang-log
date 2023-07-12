42b9a0d4e316bda567e742920f6e233200d805ac
hgo641 <hgo641@gmail.com>
2023-07-12 20:48:03 +0900

    feat: ItemType 정적 팩토리 메서드 추가

--- backend/src/main/java/hanglog/trip/domain/type/ItemType.java
+++ backend/src/main/java/hanglog/trip/domain/type/ItemType.java
@@ -1,6 +1,21 @@
 package hanglog.trip.domain.type;
 
+import java.util.Arrays;
+
 public enum ItemType {
-    SPOT,
-    NON_SPOT
+
+    SPOT(true),
+    NON_SPOT(false);
+
+    private final Boolean isSpot;
+
+    ItemType(final boolean isSpot) {
+        this.isSpot = isSpot;
+    }
+
+    public static ItemType getItemTypeByIsSpot(boolean isSpot) {
+        return Arrays.stream(ItemType.values()).filter(
+                itemType -> itemType.isSpot.equals(isSpot)
+        ).findFirst().get();
+    }
 }
