98614715a00db38af2ce0b1f956d8e46ca973b7d
hgo641 <hgo641@gmail.com>
2023-07-13 20:13:31 +0900

    feat: ItemService save 메소드 로직 변경
    Item의 ordinal 할당 로직 변경
    Item의 Place와 Expense 존재 여부를 검사하는 로직 추가

--- backend/src/main/java/hanglog/category/Category.java
+++ backend/src/main/java/hanglog/category/Category.java
@@ -6,12 +6,14 @@ import hanglog.global.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
 public class Category extends BaseEntity {
 
     @Id
--- backend/src/main/java/hanglog/trip/service/ItemService.java
+++ backend/src/main/java/hanglog/trip/service/ItemService.java
@@ -2,19 +2,23 @@ package hanglog.trip.service;
 
 import hanglog.category.Category;
 import hanglog.category.repository.CategoryRepository;
+import hanglog.expense.Expense;
 import hanglog.trip.domain.DayLog;
 import hanglog.trip.domain.Item;
 import hanglog.trip.domain.Place;
 import hanglog.trip.domain.repository.DayLogRepository;
 import hanglog.trip.domain.repository.ItemRepository;
 import hanglog.trip.domain.type.ItemType;
+import hanglog.trip.presentation.dto.request.ExpenseRequest;
 import hanglog.trip.presentation.dto.request.ItemRequest;
 import hanglog.trip.presentation.dto.request.PlaceRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ItemService {
 
     private final ItemRepository itemRepository;
@@ -23,27 +27,63 @@ public class ItemService {
 
     public Long save(final Long tripId, final ItemRequest itemRequest) {
         // TODO: 유저 인가 로직 필요
-        PlaceRequest placeRequest = itemRequest.getPlace();
-        Category category = categoryRepository.findById(placeRequest.getCategoryId())
+        DayLog dayLog = dayLogRepository.findById(itemRequest.getDayLogId())
+                .orElseThrow(() -> new IllegalArgumentException("요청한 ID에 해당하는 데이로그가 존재하지 않습니다."));
+
+        Item item = new Item(
+                ItemType.getItemTypeByIsSpot(itemRequest.getItemType()),
+                itemRequest.getTitle(),
+                getNewItemOrdinal(tripId),
+                itemRequest.getRating(),
+                itemRequest.getMemo(),
+                getPlaceByItemRequest(itemRequest),
+                dayLog,
+                getExpenseByItemRequest(itemRequest)
+        );
+
+        return itemRepository.save(item).getId();
+    }
+
+    private Place getPlaceByItemRequest(final ItemRequest itemRequest) {
+        if (itemRequest.getPlace() == null) {
+            return null;
+        }
+        return createPlaceByPlaceRequest(itemRequest.getPlace());
+    }
+
+    private Place createPlaceByPlaceRequest(final PlaceRequest placeRequest) {
+        Category category = categoryRepository.findByGoogleApiId(placeRequest.getCategoryApiId())
                 .orElseThrow(() -> new IllegalArgumentException("요청한 ID에 해당하는 카테고리가 존재하지 않습니다."));
 
-        Place place = new Place(placeRequest.getName(),
+        return new Place(
+                placeRequest.getName(),
                 placeRequest.getAddress(),
                 placeRequest.getLatitude(),
                 placeRequest.getLongitude(),
                 category);
+    }
 
-        DayLog dayLog = dayLogRepository.findById(itemRequest.getDayLogId())
-                .orElseThrow(() -> new IllegalArgumentException("요청한 ID에 해당하는 데이로그가 존재하지 않습니다."));
+    private Expense getExpenseByItemRequest(final ItemRequest itemRequest) {
+        if (itemRequest.getExpense() == null) {
+            return null;
+        }
+        return createExpenseByExpenseRequest(itemRequest.getExpense());
+    }
 
-        Item item = new Item(ItemType.getItemTypeByIsSpot(itemRequest.getItemType()),
-                itemRequest.getTitle(),
-                itemRequest.getOrdinal(),
-                itemRequest.getRating(),
-                itemRequest.getMemo(),
-                place,
-                dayLog
+    private Expense createExpenseByExpenseRequest(final ExpenseRequest expenseRequest) {
+        final Category expenseCategory = categoryRepository.findById(expenseRequest.getCategoryId())
+                .orElseThrow(() -> new IllegalArgumentException("요청한 ID에 해당하는 카테고리가 존재하지 않습니다."));
+        return new Expense(
+                expenseRequest.getCurrency(),
+                expenseRequest.getAmount(),
+                expenseCategory
         );
-        return itemRepository.save(item).getId();
+    }
+
+    private int getNewItemOrdinal(final Long dayLogId) {
+        return dayLogRepository.findById(dayLogId)
+                .orElseThrow(() -> new IllegalArgumentException("요청한 ID에 해당하는 데이로그가 존재하지 않습니다."))
+                .getItems()
+                .size() + 1;
     }
 }
