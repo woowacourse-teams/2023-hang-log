42a5665cc75f5c5c666b2fe2712377e2c20a6f06
hgo641 <hgo641@gmail.com>
2023-07-13 20:10:54 +0900

    feat: CategoryRepository에 findByGoogleApiId 메소드 추가

--- backend/src/main/java/hanglog/category/repository/CategoryRepository.java
+++ backend/src/main/java/hanglog/category/repository/CategoryRepository.java
@@ -1,7 +1,9 @@
 package hanglog.category.repository;
 
 import hanglog.category.Category;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByGoogleApiId(String googleApiId);
 }
