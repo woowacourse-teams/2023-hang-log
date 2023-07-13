370e2141edbba84ea9609a7a91c089e83b06d29a
hgo641 <hgo641@gmail.com>
2023-07-13 20:07:27 +0900

    test: ExpenseFixture 생성

--- backend/src/test/java/hanglog/trip/fixture/ItemFixture.java
+++ backend/src/test/java/hanglog/trip/fixture/ItemFixture.java
@@ -7,15 +7,19 @@ import hanglog.trip.domain.type.ItemType;
 public class ItemFixture {
 
     // TODO: ItemFixture를 위한 임시 dayLog. 이후 dayLogFixture로 분리 필요
-    private static DayLog dayLog = new DayLog("첫날",
+    private static DayLog dayLog = new DayLog(
+            "첫날",
             1,
             TripFixture.LONDON_TRIP);
-    public static Item LONDON_EYE_ITEM = new Item(1L,
+    public static Item LONDON_EYE_ITEM = new Item(
+            1L,
             ItemType.SPOT,
             "런던 아이",
             1,
             4.5,
             "런던 아이 메모",
             PlaceFixture.LONDON_EYE,
-            dayLog);
+            dayLog,
+            ExpenseFixture.expense
+    );
 }
