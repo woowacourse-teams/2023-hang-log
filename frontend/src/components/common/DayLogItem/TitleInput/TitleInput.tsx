2491a68b2e2c285df87e90f752bec595a8bfcc44
Ashley Heo <ashleysyheo@gmail.com>
2023-07-15 18:54:42 +0900

    feat: TitleInput 컴포넌트 구현

---
+++ frontend/src/components/common/DayLogItem/TitleInput/TitleInput.tsx
@@ -0,0 +1,24 @@
+import type { ChangeEvent } from 'react';
+import { useState } from 'react';
+
+import { inputStyling } from '@components/common/DayLogItem/TitleInput/TitleInput.style';
+
+interface TitleInputProps {
+  initialTitle: string;
+}
+
+const TitleInput = ({ initialTitle }: TitleInputProps) => {
+  const [title, setTitle] = useState(initialTitle);
+
+  const handleTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  // input onBlur시에 api 요청 보내기
+
+  return (
+    <input css={inputStyling} value={title} placeholder="소제목" onChange={handleTitleChange} />
+  );
+};
+
+export default TitleInput;
