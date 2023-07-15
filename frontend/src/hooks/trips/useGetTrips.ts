2f850f5578a56142b62172fb071e9e1aef12dbc9
dladncks1217 <dlaxodud1217@gmail.com>
2023-07-15 17:00:49 +0900

    feat: Trips 가져오기 위한 useGetTrips Hook 추가

---
+++ frontend/src/hooks/trips/useGetTrips.ts
@@ -0,0 +1,14 @@
+import { END_POINTS } from '@constants/api';
+import { useQuery } from '@tanstack/react-query';
+
+import { axiosInstance } from '@api/axiosInstance';
+
+const fetchTrips = async () => {
+  return await axiosInstance.get(END_POINTS.TRIPS);
+};
+
+export const useGetTrips = () => {
+  const { isLoading, isFetching, data, isSuccess } = useQuery(['trips'], fetchTrips);
+
+  return { isLoading, isFetching, data, isSuccess };
+};
