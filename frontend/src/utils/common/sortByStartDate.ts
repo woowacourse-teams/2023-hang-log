2da328656b9c93b0c801173a718125db87704edf
dladncks1217 <dlaxodud1217@gmail.com>
2023-07-16 21:22:11 +0900

    feat: 날짜 정렬 유틸함수 추가

---
+++ frontend/src/utils/common/sortByStartDate.ts
@@ -0,0 +1,7 @@
+import { TripsType } from '@type/trips';
+
+export const sortByStartDate = (a: TripsType, b: TripsType) => {
+  const dateA = new Date(a.startDate);
+  const dateB = new Date(b.startDate);
+  return dateA.getTime() - dateB.getTime();
+};
