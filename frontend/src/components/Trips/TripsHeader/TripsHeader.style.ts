e0665d50404d56975ab0b8c39fcd8ea2322e20d1
dladncks1217 <dlaxodud1217@gmail.com>
2023-07-16 15:47:58 +0900

    refactor: Trips Header svg 위치 조정

---
+++ frontend/src/components/Trips/TripsHeader/TripsHeader.style.ts
@@ -0,0 +1,6 @@
+import { css } from '@emotion/react';
+
+export const SVGBoxStyling = css({
+  display: 'flex',
+  justifyContent: 'right',
+});
--- frontend/src/components/Trips/TripsHeader/TripsHeader.tsx
+++ frontend/src/components/Trips/TripsHeader/TripsHeader.tsx
@@ -1,17 +1,21 @@
 import TripsHeaderSVG from '@assets/svg/TripsHeader.svg';
 import { Box, Center, Flex, Heading } from 'hang-log-design-system';
 
+import { SVGBoxStyling } from './TripsHeader.style';
+
 const TripsHeader = () => {
   return (
     <>
       <Flex styles={{ justify: 'space-between', width: '100%', gap: '200px' }}>
         <Box styles={{ width: '30%', height: '18%' }}>
-          <Center>
+          <Box
+            style={{ height: '200px', paddingLeft: '50px', display: 'flex', alignItems: 'center' }}
+          >
             <Heading size="large">라곤의 여행</Heading>
-          </Center>
+          </Box>
         </Box>
-        <Box styles={{ width: '70%', height: '18%' }}>
-          <TripsHeaderSVG style={{ width: '100%' }} />
+        <Box css={SVGBoxStyling} styles={{ width: '70%', height: '18%', paddingRight: '50px' }}>
+          <TripsHeaderSVG style={{ width: '854px' }} />
         </Box>
       </Flex>
     </>
