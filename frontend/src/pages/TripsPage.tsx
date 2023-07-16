6b7a753cb6e868bfed4ac39ee856ad050a021987
dladncks1217 <dlaxodud1217@gmail.com>
2023-07-16 21:23:06 +0900

    feat: Trips 페이지 추가

--- frontend/src/pages/TripsPage.tsx
+++ frontend/src/pages/TripsPage.tsx
@@ -1,3 +1,8 @@
+import { useSelect } from 'hang-log-design-system';
+import { useEffect } from 'react';
+
+import { sortByStartDate } from '@utils/common/sortByStartDate';
+
 import { useGetTrips } from '@hooks/trips/useGetTrips';
 
 import Header from '@components/layout/Header/Header';
@@ -6,13 +11,25 @@ import TripsItemList from '@components/trips/TripsItemList/TripsItemList';
 import TripsItemNone from '@components/trips/TripsItemNone/TripsItemNone';
 
 const TripsPage = () => {
-  const { data } = useGetTrips();
+  const { data, refetch } = useGetTrips();
+  const { selected, handleSelectClick } = useSelect('등록순');
+
+  useEffect(() => {
+    if (selected === '등록순') refetch();
+  }, [selected, refetch]);
+
+  const sortedTrips =
+    selected === '날짜순' ? data?.data.trips.slice().sort(sortByStartDate) : data?.data.trips;
 
   return (
     <>
       <Header />
       <TripsHeader />
-      {data?.data.trips.length ? <TripsItemList data={data?.data.trips} /> : <TripsItemNone />}
+      {data?.data.trips.length ? (
+        <TripsItemList data={sortedTrips} sortBy={selected} changeSelect={handleSelectClick} />
+      ) : (
+        data && <TripsItemNone />
+      )}
     </>
   );
 };
