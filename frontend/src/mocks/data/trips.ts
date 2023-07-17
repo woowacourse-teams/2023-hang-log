0d306a6f43a11b07b4f261daf041604cd047cd74
dladncks1217 <dlaxodud1217@gmail.com>
2023-07-17 21:47:07 +0900

    refactor: msw mock data 분리

---
+++ frontend/src/mocks/data/trips.ts
@@ -0,0 +1,136 @@
+export const tripsMock = [
+  {
+    id: 1,
+    title: '런던 여행',
+    imageUrl: 'https://res.klook.com/image/upload/Mobile/City/n9sn4fajwa1skldmdeex.jpg',
+    cities: [
+      {
+        id: 1,
+        name: '런던',
+      },
+      {
+        id: 2,
+        name: '애든버러',
+      },
+      {
+        id: 3,
+        name: '애든버러',
+      },
+      {
+        id: 4,
+        name: '애든버러',
+      },
+      {
+        id: 5,
+        name: '애든버러',
+      },
+    ],
+    startDate: '2023-06-13',
+    endDate: '2023-06-20',
+    description:
+      '어쩌구 저쩌구 좌충우돌 라곤의 런던 여행기 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 하하하',
+  },
+  {
+    id: 2,
+    title: '서울 여행',
+    imageUrl:
+      'https://images.squarespace-cdn.com/content/v1/586ebc34d482e9c69268b69a/1624386887478-9Z3XA27D8WFVDWKW00QS/20201230173806551_JRT8E1VC.png',
+    cities: [
+      {
+        id: 1,
+        name: '대한민국',
+      },
+      {
+        id: 2,
+        name: '서울',
+      },
+    ],
+    startDate: '2023-07-13',
+    endDate: '2023-07-20',
+    description: '어쩌구 저쩌구 좌충우돌 라곤의 서울 여행기',
+  },
+  {
+    id: 3,
+    title: '강릉 여행',
+    imageUrl:
+      'https://a.cdn-hotels.com/gdcs/production144/d992/418cd5c1-7f91-4c44-9f39-3016b033eaa1.jpg?impolicy=fcrop&w=800&h=533&q=medium',
+    cities: [
+      {
+        id: 1,
+        name: '대한민국',
+      },
+      {
+        id: 2,
+        name: '강원도',
+      },
+      {
+        id: 3,
+        name: '강릉',
+      },
+    ],
+    startDate: '2021.01.13',
+    endDate: '2021.01.17',
+    description: '어쩌구 저쩌구 좌충우돌 라곤의 강릉 여행기',
+  },
+  {
+    id: 4,
+    title: '바르셀로나 여행',
+    imageUrl:
+      'https://www.jamonfive.com/files/attach/images/1032/015/002/aa092a675ca23b89e818562389d62c12.jpg',
+    cities: [
+      {
+        id: 1,
+        name: '스페인',
+      },
+      {
+        id: 2,
+        name: '바르셀로나',
+      },
+    ],
+    startDate: '2015.07.13',
+    endDate: '2021.-0.-28',
+    description: '어쩌구 저쩌구 좌충우돌 레고의 바르셀로나 여행기',
+  },
+  {
+    id: 5,
+    title: '어바인 여행',
+    imageUrl:
+      'https://youimg1.tripcdn.com/target/10060n000000e4c6nEB47_D_1180_558.jpg?proc=source%2Ftrip',
+    cities: [
+      {
+        id: 1,
+        name: '미국',
+      },
+      {
+        id: 2,
+        name: '캘리포니아',
+      },
+      {
+        id: 2,
+        name: '어바인',
+      },
+    ],
+    startDate: '2023-03-31',
+    endDate: '2023-04-20',
+    description: '어쩌구 저쩌구 좌충우돌 첵스의 어바인 여행기',
+  },
+  {
+    id: 6,
+    title: 'LA 여행',
+    imageUrl:
+      'https://www.discoverlosangeles.com/sites/default/files/images/2023-02/IMG_0410-Edit-3.jpg?width=1600&height=1200&fit=crop&quality=78&auto=webp',
+    cities: [
+      {
+        id: 1,
+        name: '미국',
+      },
+      {
+        id: 2,
+        name: 'LA',
+      },
+    ],
+    startDate: '2018.08.15',
+    endDate: '2018.08.30',
+    description: '어쩌구 저쩌구 좌충우돌 야미의 LA 여행기',
+  },
+];
