e9120f49cbf0490e154f89a7575848d6ab4e83d0
waterricecake <fruturum@nate.com>
2023-07-18 12:46:51 +0900

    refactor: 불필요한 예외처리 제거

--- backend/src/main/java/hanglog/member/exception/AlreadyExistUserException.java
+++
@@ -1,8 +0,0 @@
-package hanglog.member.exception;
-
-public class AlreadyExistUserException extends UserAuthorizeException {
-
-    public AlreadyExistUserException(String message) {
-        super(message);
-    }
-}
--- backend/src/main/java/hanglog/member/exception/UserAuthorizeException.java
+++
@@ -1,8 +0,0 @@
-package hanglog.member.exception;
-
-public abstract class UserAuthorizeException extends RuntimeException {
-
-    public UserAuthorizeException(String message) {
-        super(message);
-    }
-}
