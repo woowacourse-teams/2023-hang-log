5ddc863d294eb1133ea8758ce6872fa1205005d6
mcodnjs <codnjs8989@gmail.com>
2023-07-19 20:10:17 +0900

    feat: ExceptionCode 추가

--- backend/src/main/java/hanglog/global/exception/ExceptionCode.java
+++ backend/src/main/java/hanglog/global/exception/ExceptionCode.java
@@ -9,8 +9,17 @@ import lombok.RequiredArgsConstructor;
 public enum ExceptionCode {
 
     INVALID_REQUEST(0000, "올바르지 않은 요청입니다."),
-    NOT_FOUND_DAY_LOG_ID(1000, "요청한 ID에 해당하는 데이로그가 존재하지 않습니다."),
-    ALREADY_DELETED_DATE(1001, "이미 삭제된 날짜입니다.");
+
+    NOT_FOUND_TRIP_ID(1001, "요청한 ID에 해당하는 여행이 존재하지 않습니다."),
+    NOT_FOUND_DAY_LOG_ID(1002, "요청한 ID에 해당하는 데이로그가 존재하지 않습니다."),
+    NOT_FOUND_TRIP_ITEM_ID(1003, "요청한 ID에 해당하는 여행 아이템이 존재하지 않습니다."),
+    NOT_FOUND_EXPENSE_ID(1004, "요청한 ID에 해당하는 금액이 존재하지 않습니다."),
+    NOT_FOUND_CITY_ID(1005, "요청한 ID에 해당하는 도시가 존재하지 않습니다."),
+    NOT_FOUND_PLACE_ID(1006, "요청한 ID에 해당하는 장소가 존재하지 않습니다."),
+    NOT_FOUND_CATEGORY_ID(1007, "요청한 ID에 해당하는 카테고리가 존재하지 않습니다."),
+
+    ALREADY_DELETED_TRIP_ITEM(2001, "이미 삭제된 여행 아이템입니다."),
+    ALREADY_DELETED_DATE(2002, "이미 삭제된 날짜입니다.");
 
     private final int code;
     private final String message;
