e02638c5a559c7695263f4c679477e6c772c9acd
waterricecake <fruturum@nate.com>
2023-07-20 11:04:51 +0900

    refactor: OAuth 제공자 Controller에서 분류

--- backend/src/main/java/hanglog/member/controller/OAuthLoginController.java
+++ backend/src/main/java/hanglog/member/controller/OAuthLoginController.java
@@ -1,10 +1,9 @@
 package hanglog.member.controller;
 
+import hanglog.member.Member;
 import hanglog.member.service.LoginService;
-import hanglog.member.service.Provider;
 import hanglog.member.service.dto.UserInfoDto;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +12,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/login/oauth2")
 public class OAuthLoginController {
 
-    private final Provider provider;
+    private final LoginService googleLoginService;
+    private final LoginService kakaoLoginService;
 
-    public OAuthLoginController(final Provider provider) {
-        this.provider = provider;
+    public OAuthLoginController(final LoginService googleLoginService, final LoginService kakaoLoginService) {
+        this.googleLoginService = googleLoginService;
+        this.kakaoLoginService = kakaoLoginService;
     }
 
-    @GetMapping("/code/{oAuthProvider}")
-    public void loginOAuth(@RequestParam final String code, @PathVariable final String oAuthProvider) {
-        final LoginService loginService = provider.mappingOAuthProvider(oAuthProvider);
+    @GetMapping("/code/google")
+    public void googleLoginOAuth(@RequestParam final String code) {
+        loginOAuth(googleLoginService, code);
+    }
+
+    @GetMapping("/code/kakao")
+    public void kakaoLoginOAuth(@RequestParam final String code) {
+        loginOAuth(kakaoLoginService, code);
+    }
+
+    private Member loginOAuth(final LoginService loginService, final String code) {
         final String accessCode = loginService.getAccessToken(code);
         final UserInfoDto userInfo = loginService.getUserInfo(accessCode);
-        loginService.socialLogin(userInfo.getId(), userInfo.getNickname(), userInfo.getImageUrl());
+        return loginService.socialLogin(userInfo.getId(), userInfo.getNickname(), userInfo.getImageUrl());
     }
 }
