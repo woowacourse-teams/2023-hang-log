27a881aa278fa93c47cb4865a57fd79e7b65f1db
Ashley Heo <ashleysyheo@gmail.com>
2023-07-21 17:05:43 +0900

    feat: useResetError 커스텀 훅 구현

---
+++ frontend/src/hooks/common/useResetError.ts
@@ -0,0 +1,29 @@
+import { HTTP_STATUS_CODE } from '@constants/api';
+import { PATH } from '@constants/path';
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { HTTPError } from '@api/HTTPError';
+
+export const useResetError = () => {
+  const navigate = useNavigate();
+
+  const handleErrorReset = useCallback(
+    (error: Error | HTTPError) => {
+      if (error instanceof Error && !(error instanceof HTTPError)) {
+        navigate(PATH.RELOAD);
+
+        return;
+      }
+
+      if (error.statusCode >= HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR) {
+        navigate(PATH.RELOAD);
+      } else {
+        navigate(PATH.ROOT);
+      }
+    },
+    [navigate]
+  );
+
+  return { handleErrorReset };
+};
