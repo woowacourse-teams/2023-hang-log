4b745fb92c9c03ab8acc594a81e74b18e39bc0d1
Ashley Heo <ashleysyheo@gmail.com>
2023-07-21 17:05:14 +0900

    feat: Error 컴포넌트 구현

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = '/';
+export const BASE_URL = 'http://3.35.232.23';
 
 export const END_POINTS = {
   TRIPS: '/trips',
@@ -7,9 +7,33 @@ export const END_POINTS = {
   DAY_LOG_ORDER: (tripId: number, dayLogId: number) => `/trips/${tripId}/daylogs/${dayLogId}/order`,
   CREATE_TRIP_ITEM: (tripId: number) => `/trips/${tripId}/items`,
   CHANGE_TRIP_ITEM: (tripId: number, itemId: number) => `/trips/${tripId}/items/${itemId}`,
-  CITIES:'/cities'
+  CITIES: '/cities',
 } as const;
 
 export const NETWORK = {
   RETRY_COUNT: 3,
+} as const;
+
+export const HTTP_STATUS_CODE = {
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+} as const;
+
+export const HTTP_ERROR_MESSAGE = {
+  [HTTP_STATUS_CODE.NOT_FOUND]: {
+    HEADING: '길을 잃었나요?',
+    BODY: '요청하신 페이지를 찾을 수 없습니다.',
+    BUTTON: '홈으로 가기',
+  },
+  [HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR]: {
+    HEADING: '현재 페이지를 표시할 수 없습니다.',
+    BODY: `잠시 후 다시 시도해주세요.`,
+    BUTTON: '새로고침',
+  },
+  [HTTP_STATUS_CODE.BAD_REQUEST]: {
+    HEADING: '잘못된 요청입니다.',
+    BODY: '확인 후 다시 시도해주세요.',
+    BUTTON: '홈으로 가기',
+  },
 } as const;
