cd620f2a5612edbaab0f8663941ad06687ad2b55
waterricecake <fruturum@nate.com>
2023-07-21 13:35:23 +0900

    feat: NOT_FOUND_IMAGE_URL exceptionCod 추가

--- backend/src/main/java/hanglog/global/exception/ExceptionCode.java
+++ backend/src/main/java/hanglog/global/exception/ExceptionCode.java
@@ -17,6 +17,7 @@ public enum ExceptionCode {
     NOT_FOUND_CITY_ID(1005, "요청한 ID에 해당하는 도시가 존재하지 않습니다."),
     NOT_FOUND_PLACE_ID(1006, "요청한 ID에 해당하는 장소가 존재하지 않습니다."),
     NOT_FOUND_CATEGORY_ID(1007, "요청한 ID에 해당하는 카테고리가 존재하지 않습니다."),
+    NOT_FOUNT_IMAGE_URL(1008, "요청한 URL에 해당하는 이미지가 존재하지 않습니다."),
 
     ALREADY_DELETED_TRIP_ITEM(2001, "이미 삭제된 여행 아이템입니다."),
     ALREADY_DELETED_DATE(2002, "이미 삭제된 날짜입니다.");
