bd152797afc359717fd4462d410c9848564afe01
Ashley Heo <ashleysyheo@gmail.com>
2023-07-21 17:03:18 +0900

    feat: axios 에러 핸들링 interceptor 추가

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,7 +1,25 @@
 import { BASE_URL } from '@constants/api';
+import type { AxiosError } from 'axios';
 import axios from 'axios';
+
+import { HTTPError } from './HTTPError';
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
 });
+
+interface ErrorResponseData {
+  statusCode?: number;
+  message?: string;
+}
+
+export const handleAPIError = (error: AxiosError<ErrorResponseData>) => {
+  if (!error.response) throw Error('에러가 발생했습니다.');
+
+  const { data, status } = error.response;
+
+  throw new HTTPError(status, data.message);
+};
+
+axiosInstance.interceptors.response.use((response) => response, handleAPIError);
