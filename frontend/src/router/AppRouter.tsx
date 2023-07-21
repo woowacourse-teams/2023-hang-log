5e5f0abb1a4f4efbf46c7ef3295185c5548c90b6
Ashley Heo <ashleysyheo@gmail.com>
2023-07-21 17:06:07 +0900

    feat: NotFoundPage 페이지 구현

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -3,6 +3,7 @@ import { PATH } from '@constants/path';
 import { Suspense } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+import NotFoundPage from '@pages/NotFoundPage/NotFoundPage';
 import TripEditPage from '@pages/TripEditPage/TripEditPage';
 import TripEditPageSkeleton from '@pages/TripEditPage/TripEditPageSkeleton';
 import TripsPage from '@pages/TripsPage/TripsPage';
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
   {
     path: PATH.ROOT,
     element: <App />,
+    errorElement: <NotFoundPage />,
     children: [
       {
         path: PATH.TRIPS,
