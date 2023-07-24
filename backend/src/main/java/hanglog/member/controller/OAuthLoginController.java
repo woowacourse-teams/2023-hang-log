3ee85abfb84d8cf0bc16f157c87c1a8023ee1a82
waterricecake <fruturum@nate.com>
2023-07-24 15:52:12 +0900

    refactor: OAuth 서비스 통일

--- backend/src/main/java/hanglog/member/controller/OAuthLoginController.java
+++ backend/src/main/java/hanglog/member/controller/OAuthLoginController.java
@@ -1,9 +1,10 @@
 package hanglog.member.controller;
 
-import hanglog.member.Member;
-import hanglog.member.service.LoginService;
+import hanglog.member.provider.Provider;
+import hanglog.member.service.OAuthLoginService;
 import hanglog.member.service.dto.UserInfoDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,27 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/login/oauth2")
 public class OAuthLoginController {
 
-    private final LoginService googleLoginService;
-    private final LoginService kakaoLoginService;
+    private final OAuthLoginService oAuthLoginService;
 
-    public OAuthLoginController(final LoginService googleLoginService, final LoginService kakaoLoginService) {
-        this.googleLoginService = googleLoginService;
-        this.kakaoLoginService = kakaoLoginService;
+    public OAuthLoginController(final OAuthLoginService oAuthLoginService) {
+        this.oAuthLoginService = oAuthLoginService;
     }
 
-    @GetMapping("/code/google")
-    public void googleLoginOAuth(@RequestParam final String code) {
-        loginOAuth(googleLoginService, code);
-    }
-
-    @GetMapping("/code/kakao")
-    public void kakaoLoginOAuth(@RequestParam final String code) {
-        loginOAuth(kakaoLoginService, code);
-    }
-
-    private Member loginOAuth(final LoginService loginService, final String code) {
-        final String accessCode = loginService.getAccessToken(code);
-        final UserInfoDto userInfo = loginService.getUserInfo(accessCode);
-        return loginService.socialLogin(userInfo.getId(), userInfo.getNickname(), userInfo.getImageUrl());
+    @GetMapping("/code/{oAuthProvider}")
+    public void googleLoginOAuth(@RequestParam final String code, @PathVariable final String oAuthProvider) {
+        final Provider provider = Provider.mappingProvider(oAuthProvider);
+        final String accessCode = oAuthLoginService.getAccessToken(code, provider.getProperties());
+        final UserInfoDto userInfo = oAuthLoginService.getUserInfo(accessCode, provider);
+        oAuthLoginService.socialLogin(userInfo.getId(), userInfo.getNickname(), userInfo.getImageUrl());
     }
 }
--- backend/src/main/java/hanglog/member/service/OAuthLoginService.java
+++ backend/src/main/java/hanglog/member/service/OAuthLoginService.java
@@ -1,6 +1,8 @@
 package hanglog.member.service;
 
 import hanglog.member.Member;
+import hanglog.member.provider.Provider;
+import hanglog.member.provider.ProviderProperties;
 import hanglog.member.repository.MemberRepository;
 import hanglog.member.service.dto.AccessTokenResponseDto;
 import hanglog.member.service.dto.UserInfoDto;
@@ -9,64 +11,60 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-public abstract class LoginService {
-
-    protected final RestTemplate restTemplate;
-    protected final String clientId;
-    protected final String clientSecret;
-    protected final String redirectUri;
-    protected final String tokenUri;
-    protected final String userUri;
+@Service
+public class OAuthLoginService {
     private final MemberRepository memberRepository;
+    private final RestTemplate restTemplate;
 
-    public LoginService(
-            final MemberRepository memberRepository,
-            final String clientId,
-            final String clientSecret,
-            final String redirectUri,
-            final String tokenUri,
-            final String userUri
-    ) {
+    public OAuthLoginService(final MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
         this.restTemplate = new RestTemplate();
-        this.clientId = clientId;
-        this.clientSecret = clientSecret;
-        this.redirectUri = redirectUri;
-        this.tokenUri = tokenUri;
-        this.userUri = userUri;
     }
 
-    public String getAccessToken(final String code) {
+    public String getAccessToken(final String code, final ProviderProperties properties) {
+        System.out.println(properties.getTokenUri());
         final ResponseEntity<AccessTokenResponseDto> accessTokenResponse = restTemplate.exchange(
-                tokenUri,
+                properties.getTokenUri(),
                 HttpMethod.POST,
-                getEntity(code),
+                getEntity(code, properties),
                 AccessTokenResponseDto.class
         );
         // TODO : token get 실패시 에러처리
         return accessTokenResponse.getBody().getAccessToken();
     }
 
-    abstract public UserInfoDto getUserInfo(final String accessToken);
+    public UserInfoDto getUserInfo(final String accessToken, final Provider provider) {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+        final HttpEntity entity = new HttpEntity(headers);
+        return (UserInfoDto) restTemplate.exchange(
+                provider.getProperties().getUserUri(),
+                HttpMethod.GET,
+                entity,
+                provider.getResponseDto()
+        ).getBody();
 
-    // TODO : 토큰 발기
+    }
+
+    // TODO : 토큰 받기
     @Transactional
     public Member socialLogin(final String socialLoginId, final String nickname, final String image) {
         return memberRepository.findBySocialLoginId(socialLoginId)
                 .orElseGet(() -> saveMember(socialLoginId, nickname, image));
     }
 
-    private HttpEntity getEntity(final String code) {
+    private HttpEntity getEntity(final String code, final ProviderProperties properties) {
         final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("code", code);
-        params.add("client_id", clientId);
-        params.add("client_secret", clientSecret);
-        params.add("redirect_uri", redirectUri);
+        params.add("client_id", properties.getClientId());
+        params.add("client_secret", properties.getClientSecret());
+        params.add("redirect_uri", properties.getRedirectUri());
         params.add("grant_type", "authorization_code");
 
         final HttpHeaders headers = new HttpHeaders();
