eff42b331bc9387f534c539a41b4db763de88040
Dahye <06robin11@gmail.com>
2023-07-25 16:29:12 +0900

    refactor:함수형컴포넌트 수정

--- frontend/src/hooks/common/useDebounce.ts
+++ frontend/src/hooks/common/useDebounce.ts
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 
-function useDebounce<T>(value: T, delay?: number): T {
+export const useDebounce = <T>(value: T, delay: number = 500): T => {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
 
     return () => {
       clearTimeout(timer);
@@ -12,6 +12,4 @@ function useDebounce<T>(value: T, delay?: number): T {
   }, [value, delay]);
 
   return debouncedValue;
-}
-
-export default useDebounce;
+};
