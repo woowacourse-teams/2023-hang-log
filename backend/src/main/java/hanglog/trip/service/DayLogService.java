d31f14d04b94630cc0fb4efd54f20bc3015ddb93
hgo641 <hgo641@gmail.com>
2023-07-25 11:16:55 +0900

    refactor: DayLog의 Item들 순서를 변경하는 로직 예외처리 추가

--- backend/src/main/java/hanglog/trip/service/DayLogService.java
+++ backend/src/main/java/hanglog/trip/service/DayLogService.java
@@ -2,8 +2,10 @@ package hanglog.trip.service;
 
 
 import static hanglog.global.exception.ExceptionCode.ALREADY_DELETED_DATE;
+import static hanglog.global.exception.ExceptionCode.INVALID_ORDERED_ITEM_IDS;
 import static hanglog.global.exception.ExceptionCode.NOT_FOUND_DAY_LOG_ID;
 import static hanglog.global.exception.ExceptionCode.NOT_FOUND_TRIP_ID;
+import static hanglog.global.exception.ExceptionCode.NOT_FOUND_TRIP_ITEM_ID;
 
 import hanglog.global.exception.BadRequestException;
 import hanglog.trip.domain.DayLog;
@@ -62,19 +64,25 @@ public class DayLogService {
                 .orElseThrow(() -> new BadRequestException(NOT_FOUND_DAY_LOG_ID));
         final List<Item> items = dayLog.getItems();
 
-        // TODO: orders가 item들 수만큼 있는지 valid - custom exeption 추가 필요
-        if (itemsOrdinalUpdateRequest.getItemIds().size() != items.size()) {
-            throw new IllegalArgumentException("데이로그에 있는 모든 아이템들에 대한 아이디 리스트가 필요합니다.");
-        }
+        final List<Long> orderedItemIds = itemsOrdinalUpdateRequest.getItemIds();
+        validateOrderedItemIds(items, orderedItemIds);
+        changeOrdinalOfItemsByOrderedItemIds(orderedItemIds);
+    }
 
-        changeOrdinalOfItemsByOrderedItemIds(itemsOrdinalUpdateRequest.getItemIds());
+    private void validateOrderedItemIds(final List<Item> items, final List<Long> orderedItemIds) {
+        for (final Item item : items) {
+            if (!orderedItemIds.contains(item.getId())) {
+                throw new BadRequestException(INVALID_ORDERED_ITEM_IDS);
+            }
+        }
     }
 
     private void changeOrdinalOfItemsByOrderedItemIds(final List<Long> orderedItemIds) {
         int ordinal = 1;
 
         for (final Long itemId : orderedItemIds) {
-            final Item item = itemRepository.findById(itemId).get();
+            final Item item = itemRepository.findById(itemId)
+                    .orElseThrow(() -> new BadRequestException(NOT_FOUND_TRIP_ITEM_ID));
             item.changeOrdinal(ordinal++);
         }
     }
