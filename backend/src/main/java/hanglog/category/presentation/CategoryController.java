97bde5af5522d60dfd7cfc1f045ac233697ba086
jjongwa <troas96@naver.com>
2023-07-27 14:12:11 +0900

    feat: expenseCategory 조회 기능 Controller 구현

--- backend/src/main/java/hanglog/category/presentation/CategoryController.java
+++ backend/src/main/java/hanglog/category/presentation/CategoryController.java
@@ -1,4 +1,24 @@
 package hanglog.category.presentation;
 
+import hanglog.category.dto.CategoryResponse;
+import hanglog.category.service.CategoryService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getExpenseCategories() {
+        final List<CategoryResponse> categoryResponses = categoryService.getExpenseCategories();
+        return ResponseEntity.ok(categoryResponses);
+    }
 }
