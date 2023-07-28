0e544dfefe7fc257cbb77f9180b514d8ccd143ea
Dahye <06robin11@gmail.com>
2023-07-28 17:00:16 +0900

    refactor:여행생성시 서버에서 받는 location에 따른 로직 변경

--- frontend/src/api/trip/postTrip.ts
+++ frontend/src/api/trip/postTrip.ts
@@ -6,5 +6,7 @@ import { axiosInstance } from '@api/axiosInstance';
 export const postTrip = () => async (newTripData: NewTripData) => {
   const response = await axiosInstance.post(END_POINTS.TRIPS, newTripData);
 
-  return response.headers.location;
+  const tripId = response.headers.location.replace(`${END_POINTS.TRIPS}/`, '');
+
+  return tripId;
 };
