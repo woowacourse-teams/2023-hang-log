83d0d140b2e829a99d2751fb7948155285b1c568
LJW25 <dbjwlee@gmail.com>
2023-07-28 15:57:24 +0900

    feat: 이미지 예외 처리 구현
    Co-authored-by: hgo641 <hgo641@gmail.com>

--- backend/src/main/java/hanglog/global/exception/ExceptionCode.java
+++ backend/src/main/java/hanglog/global/exception/ExceptionCode.java
@@ -24,7 +24,13 @@ public enum ExceptionCode {
 
     INVALID_RATING(3001, "별점은 N.0점이거나 N.5점 형태이어야 합니다."),
 
-    INVALID_ORDERED_ITEM_IDS(4001, "날짜에 속한 모든 여행 아이템들의 ID가 필요합니다.");
+    INVALID_ORDERED_ITEM_IDS(4001, "날짜에 속한 모든 여행 아이템들의 ID가 필요합니다."),
+
+    IMAGE_SIZE_EXCEED(5001, "업로드 가능한 이미지 용량을 초과했습니다."),
+    IMAGE_NULL(5002, "업로드한 이미지 파일이 NULL입니다."),
+    IMAGE_LIST_EMPTY(5003, "최소 한 장 이상의 이미지를 업로드해야합니다."),
+    IMAGE_LIST_SIZE_EXCEED(5004, "업로드 가능한 이미지 개수를 초과했습니다."),
+    IMAGE_PATH_INVALID(5100, "이미지를 저장할 경로가 올바르지 않습니다.");
 
     private final int code;
     private final String message;
--- backend/src/main/java/hanglog/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/hanglog/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package hanglog.global.exception;
 
+import static hanglog.global.exception.ExceptionCode.IMAGE_SIZE_EXCEED;
+
+import java.util.Objects;
+import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -9,15 +13,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Objects;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     // TODO: Logger 달기
 
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex, final HttpHeaders headers, final HttpStatusCode status, final WebRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex,
+                                                                  final HttpHeaders headers,
+                                                                  final HttpStatusCode status,
+                                                                  final WebRequest request) {
         final String errMessage = Objects.requireNonNull(ex.getBindingResult().getFieldError()).getDefaultMessage();
         return ResponseEntity.badRequest()
                 .body(new ExceptionResponse(0000, errMessage));
@@ -27,5 +32,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<ExceptionResponse> handleBadRequestException(final BadRequestException e) {
         return ResponseEntity.badRequest()
                 .body(new ExceptionResponse(e.getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(SizeLimitExceededException.class)
+    public ResponseEntity<ExceptionResponse> handleSizeLimitExceededException(final SizeLimitExceededException e) {
+        final String message = IMAGE_SIZE_EXCEED.getMessage()
+                + " 입력된 이미지 용량은 " + e.getActualSize() + " byte 입니다. "
+                + "(제한 용량: " + e.getPermittedSize() + " byte)";
+        return ResponseEntity.badRequest()
+                .body(new ExceptionResponse(IMAGE_SIZE_EXCEED.getCode(), message));
     }
 }
