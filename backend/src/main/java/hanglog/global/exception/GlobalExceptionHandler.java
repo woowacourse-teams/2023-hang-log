fbc86622a4eb66aa45258a9c9cced81babebbf21
jjongwa <troas96@naver.com>
2023-07-29 15:31:51 +0900

    feat: Exception에 logger 추가

--- backend/src/main/java/hanglog/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/hanglog/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package hanglog.global.exception;
 
+import java.util.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -9,28 +12,34 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Objects;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    // TODO: Logger 달기
+    private static final Logger handlerLogger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex, final HttpHeaders headers, final HttpStatusCode status, final WebRequest request) {
-        final String errMessage = Objects.requireNonNull(ex.getBindingResult().getFieldError()).getDefaultMessage();
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            final MethodArgumentNotValidException e,
+            final HttpHeaders headers,
+            final HttpStatusCode status,
+            final WebRequest request
+    ) {
+        final String errMessage = Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage();
+        handlerLogger.warn(errMessage, e);
         return ResponseEntity.badRequest()
                 .body(new ExceptionResponse(0000, errMessage));
     }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ExceptionResponse> handleBadRequestException(final BadRequestException e) {
+        handlerLogger.warn(e.getMessage(), e);
         return ResponseEntity.badRequest()
                 .body(new ExceptionResponse(e.getCode(), e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleException(final Exception e) {
+        handlerLogger.error(e.getMessage(), e);
         return ResponseEntity.internalServerError()
                 .body(new ExceptionResponse(9999, e.getMessage()));
     }
