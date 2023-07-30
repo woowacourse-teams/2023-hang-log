b0ab46682b32cc7bf4ff46b57c6f6eae10dbb9ce
Ashley Heo <ashleysyheo@gmail.com>
2023-07-30 22:39:13 +0900

    refactor: TripItemMarker customization

--- frontend/src/components/common/TripItemMarker/TripItemMarker.tsx
+++ frontend/src/components/common/TripItemMarker/TripItemMarker.tsx
@@ -1,22 +1,48 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 interface TripItemMarkerProps {
   map: google.maps.Map;
   id: number;
   lat: number;
   lng: number;
+  isSelected: boolean;
 }
 
-const TripItemMarker = ({ map, id, lat, lng }: TripItemMarkerProps) => {
+const TripItemMarker = ({ map, id, lat, lng, isSelected }: TripItemMarkerProps) => {
+  const [marker, setMarker] = useState<google.maps.marker.AdvancedMarkerElement | null>(null);
+
   useEffect(() => {
     const marker = new google.maps.marker.AdvancedMarkerElement({
       position: { lat, lng },
       map,
-      title: '몰라요',
     });
-  }, [lat, lng]);
 
-  return <></>;
+    setMarker(marker);
+
+    return () => {
+      marker.map = null;
+    };
+  }, [id, lat, lng, map]);
+
+  useEffect(() => {
+    if (marker) {
+      const pin = isSelected
+        ? new google.maps.marker.PinElement({
+            glyphColor: '#D83AA2',
+            background: '#EA73C1',
+            borderColor: '#D83AA2',
+          })
+        : new google.maps.marker.PinElement({
+            glyphColor: '#0083BB',
+            background: '#26A8E0',
+            borderColor: '#0083BB',
+          });
+
+      marker.content = pin.element;
+    }
+  }, [isSelected, marker]);
+
+  return null;
 };
 
 export default TripItemMarker;
