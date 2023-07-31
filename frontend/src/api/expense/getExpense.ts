e1d699e177ed221b309e481c631d87ec67a52735
Ashley Heo <ashleysyheo@gmail.com>
2023-07-31 22:45:08 +0900

    fix: getExpense 엔드포인트 수정

--- frontend/src/api/expense/getExpense.ts
+++ frontend/src/api/expense/getExpense.ts
@@ -4,7 +4,7 @@ import type { ExpenseData } from '@type/expense';
 import { axiosInstance } from '@api/axiosInstance';
 
 export const getExpense = async (tripId: number) => {
-  const { data } = await axiosInstance.get<ExpenseData>(END_POINTS.TRIP(tripId));
+  const { data } = await axiosInstance.get<ExpenseData>(END_POINTS.EXPENSE(tripId));
 
   return data;
 };
