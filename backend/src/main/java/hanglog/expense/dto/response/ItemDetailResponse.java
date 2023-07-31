89522eaa5915a12a4e888ab955902abca3555823
waterricecake <fruturum@nate.com>
2023-07-31 20:19:49 +0900

    code: 코드 컨벤션 적용

--- backend/src/main/java/hanglog/expense/dto/response/ItemDetailResponse.java
+++ backend/src/main/java/hanglog/expense/dto/response/ItemDetailResponse.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class ItemDetailResponse {
+
     private final Long id;
     private final String title;
     private final int ordinal;
