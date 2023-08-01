ea3b6a9af5be959c4d0afb74b1bce98626dbd6eb
Ashley Heo <ashleysyheo@gmail.com>
2023-08-01 10:22:20 +0900

    refactor: ExpenseInformation 컴포넌트 스타일 수정

--- frontend/src/components/expense/ExpenseInformation/ExpenseInformation.style.ts
+++ frontend/src/components/expense/ExpenseInformation/ExpenseInformation.style.ts
@@ -5,7 +5,7 @@ export const sectionStyling = css({
   position: 'relative',
   width: '100%',
   padding: `${Theme.spacer.spacing4} 0`,
-  paddingBottom: Theme.spacer.spacing5,
+  marginBottom: Theme.spacer.spacing5,
 });
 
 export const titleStyling = css({
