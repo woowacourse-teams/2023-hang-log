06e72318e0bbdaca311058a0e00f024e78e7f901
Ashley Heo <ashleysyheo@gmail.com>
2023-08-01 16:49:23 +0900

    feat: MemoInput 컴포넌트 스토리 추가

---
+++ frontend/src/stories/trip/MemoInput.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import type { TripItemFormData } from '@type/tripItem';
+import { useState } from 'react';
+
+import MemoInput from '@components/trip/TripItemAddModal/MemoInput/MemoInput';
+
+const meta = {
+  title: 'trip/TripItemAddModal/MemoInput',
+  component: MemoInput,
+  args: {
+    value: '',
+  },
+} satisfies Meta<typeof MemoInput>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => {
+    const [value, setValue] = useState('');
+
+    const updateInputValue = <K extends keyof TripItemFormData>(
+      id: K,
+      value: TripItemFormData[K]
+    ) => {
+      setValue(value as string);
+    };
+
+    return <MemoInput value={value} updateInputValue={updateInputValue} />;
+  },
+};
