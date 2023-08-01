2ec3ca4cac23dd1b95de4a63f6964b99b9a897dd
waterricecake <fruturum@nate.com>
2023-08-01 11:44:51 +0900

    refactor: 정펙메 list 반환 제거

--- backend/src/main/java/hanglog/expense/dto/response/CurrencyRateResponse.java
+++ backend/src/main/java/hanglog/expense/dto/response/CurrencyRateResponse.java
@@ -1,9 +1,5 @@
 package hanglog.expense.dto.response;
 
-import hanglog.expense.domain.Currency;
-import hanglog.expense.domain.type.CurrencyType;
-import java.util.Arrays;
-import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -13,10 +9,4 @@ public class CurrencyRateResponse {
 
     private final String currency;
     private final double rate;
-
-    public static List<CurrencyRateResponse> of(final Currency currency) {
-        return Arrays.stream(CurrencyType.values())
-                .map(value -> new CurrencyRateResponse(value.getCode(), value.getGetRate().apply(currency)))
-                .toList();
-    }
 }
