e88f0ff97b5f17cb8dd17d071e5872b8c733af86
Ashley Heo <ashleysyheo@gmail.com>
2023-08-01 13:23:29 +0900

    fix: import 버그 해결

--- frontend/src/mocks/handlers/city.ts
+++ frontend/src/mocks/handlers/city.ts
@@ -1,9 +1,9 @@
 import { END_POINTS } from '@constants/api';
-import { city } from '@mocks/data/city';
+import { cities } from '@mocks/data/city';
 import { rest } from 'msw';
 
 export const cityHandlers = [
   rest.get(END_POINTS.CITY, (_, res, ctx) => {
-    return res(ctx.status(200), ctx.json(city));
+    return res(ctx.status(200), ctx.json(cities));
   }),
 ];
