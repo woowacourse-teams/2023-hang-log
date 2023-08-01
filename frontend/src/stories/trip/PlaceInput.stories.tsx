20dca85499a39e830df766440f714df11d9d5a39
Ashley Heo <ashleysyheo@gmail.com>
2023-08-01 19:18:57 +0900

    feat: PlaceInput 컴포넌트 스토리 추가

---
+++ frontend/src/stories/trip/PlaceInput.stories.tsx
@@ -0,0 +1,45 @@
+import GoogleMapWrapper from '@/components/common/GoogleMapWrapper/GoogleMapWrapper';
+import type { Meta, StoryObj } from '@storybook/react';
+import type { TripItemFormData } from '@type/tripItem';
+import { useState } from 'react';
+
+import PlaceInput from '@components/trip/TripItemAddModal/PlaceInput/PlaceInput';
+
+const meta = {
+  title: 'trip/TripItemAddModal/PlaceInput',
+  component: PlaceInput,
+  argTypes: {
+    isUpdatable: { control: false },
+  },
+  args: {
+    value: '',
+    isError: false,
+    isUpdatable: false,
+    disableError: () => {},
+  },
+  decorators: [
+    (Story) => (
+      <GoogleMapWrapper>
+        <Story />
+      </GoogleMapWrapper>
+    ),
+  ],
+} satisfies Meta<typeof PlaceInput>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: ({ ...args }) => {
+    const [value, setValue] = useState('');
+
+    const updateInputValue = <K extends keyof TripItemFormData>(
+      id: K,
+      value: TripItemFormData[K]
+    ) => {
+      setValue(value as string);
+    };
+
+    return <PlaceInput {...args} value={value} updateInputValue={updateInputValue} />;
+  },
+};
