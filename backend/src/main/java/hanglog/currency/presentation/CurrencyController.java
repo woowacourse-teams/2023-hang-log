dca2992f739079525cd0f07de8ad9c017e287f05
waterricecake <fruturum@nate.com>
2023-08-07 19:40:04 +0900

    feat: 당일 환율 데이터 저장 기능 구현

---
+++ backend/src/main/java/hanglog/currency/presentation/CurrencyController.java
@@ -0,0 +1,24 @@
+package hanglog.currency.presentation;
+
+import hanglog.currency.domain.Currency;
+import hanglog.currency.service.CurrencyService;
+import java.time.LocalDate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/currency")
+public class CurrencyController {
+
+    private final CurrencyService currencyService;
+
+    @PostMapping("/today")
+    public ResponseEntity<LocalDate> getTodayCurrency() {
+        final Currency currency = currencyService.saveDailyCurrency(LocalDate.now());
+        return ResponseEntity.ok(currency.getDate());
+    }
+}
