50dc6f7cc306ae7f9adeb35025d1581d316c4c0c
Dahye <06robin11@gmail.com>
2023-08-09 10:09:12 +0900

    feat:페이지전환시 화면 최상단으로 랜더링

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { useResetError } from '@hooks/common/useResetError';
 
 import Error from '@components/common/Error/Error';
 import ErrorBoundary from '@components/common/ErrorBoundary/ErrorBoundary';
+import ScrollTop from '@components/common/ScrollTop/ScrollTop';
 import ToastContainer from '@components/common/ToastContainer/ToastContainer';
 import Footer from '@components/layout/Footer/Footer';
 import Header from '@components/layout/Header/Header';
@@ -15,6 +16,7 @@ const App = () => {
 
   return (
     <ErrorBoundary Fallback={Error} onReset={handleErrorReset}>
+      <ScrollTop />
       <Header />
       <main>
         <Outlet />
