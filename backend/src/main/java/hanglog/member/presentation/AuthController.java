c4c1c73ceef7574954503fd2f958a7f5d5232619
jjongwa <troas96@naver.com>
2023-08-10 19:13:30 +0900

    refactor: 로그인 컨트롤러 구현

--- backend/src/main/java/hanglog/member/presentation/AuthController.java
+++ backend/src/main/java/hanglog/member/presentation/AuthController.java
@@ -5,21 +5,19 @@ import hanglog.member.service.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/login/oauth2")
-public class OAuthLoginController {
+public class AuthController {
 
     private final AuthService authService;
 
-    public OAuthLoginController(final AuthService authService) {
+    public AuthController(final AuthService authService) {
         this.authService = authService;
     }
 
-    @GetMapping("/code/{provider}")
+    @GetMapping("/login/{provider}")
     public ResponseEntity<TokenResponse> loginOAuth(
             @PathVariable final String provider,
             @RequestParam final String code
