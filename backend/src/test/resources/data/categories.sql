8426b1e3623d3d0169d815f41063dab07dc5ae18
waterricecake <fruturum@nate.com>
2023-08-10 16:44:19 +0900

    refactor: 300번 추가

--- backend/src/test/resources/data/categories.sql
+++ backend/src/test/resources/data/categories.sql
@@ -5,6 +5,8 @@ VALUES (200, 'culture', '문화', now(), now(), 'USABLE');
 INSERT INTO category(id, eng_name, kor_name, created_at, modified_at, status)
 VALUES (264, 'university', '대학교', now(), now(), 'USABLE');
 INSERT INTO category(id, eng_name, kor_name, created_at, modified_at, status)
+VALUES (300, 'shopping', '쇼핑', now(), now(), 'USABLE');
+INSERT INTO category(id, eng_name, kor_name, created_at, modified_at, status)
 VALUES (400, 'accomodation', '숙박', now(), now(), 'USABLE');
 INSERT INTO category(id, eng_name, kor_name, created_at, modified_at, status)
 VALUES (500, 'transportation', '교통', now(), now(), 'USABLE');
