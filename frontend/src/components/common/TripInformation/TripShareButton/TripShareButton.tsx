50f2fa48c00d4200b29a1108dc71b9129c601238
Dahye <06robin11@gmail.com>
2023-08-10 15:14:38 +0900

    feat:url복사 성공, 실패시 토스트 띄움

--- frontend/src/components/common/TripInformation/TripShareButton/TripShareButton.tsx
+++ frontend/src/components/common/TripInformation/TripShareButton/TripShareButton.tsx
@@ -1,7 +1,11 @@
 import ShareIcon from '@assets/svg/share-icon.svg';
+import { toastListState } from '@store/toast';
 import { Flex, Menu, MenuList, useOverlay } from 'hang-log-design-system';
 import { useEffect, useState } from 'react';
 import type { ChangeEvent } from 'react';
+import { useSetRecoilState } from 'recoil';
+
+import { generateUniqueId } from '@utils/uniqueId';
 
 import { useTripShareStatusMutation } from '@hooks/api/useTripShareStatusMutation';
 
@@ -25,6 +29,7 @@ const TripShareButton = ({ tripId, sharedStatus }: TripShareButtonProps) => {
   const [isSharable, setIsSharable] = useState(sharedStatus);
   const [shareUrl, setShareUrl] = useState<string | null>(null);
   const { isOpen: isShareMenuOpen, open: openShareMenu, close: closeShareMenu } = useOverlay();
+  const setToastList = useSetRecoilState(toastListState);
 
   const communicateWithServer = (sharedStatus: boolean) => {
     patchTripSharedStatus.mutate(
@@ -34,6 +39,8 @@ const TripShareButton = ({ tripId, sharedStatus }: TripShareButtonProps) => {
       },
       {
         onSuccess: (sharedUrl: string | null) => {
+          if (!sharedUrl) return;
+
           setShareUrl(sharedUrl);
         },
       }
@@ -48,7 +55,6 @@ const TripShareButton = ({ tripId, sharedStatus }: TripShareButtonProps) => {
     const isShared = e.target.checked;
 
     setIsSharable(isShared);
-
     communicateWithServer(isShared);
   };
 
@@ -56,10 +62,24 @@ const TripShareButton = ({ tripId, sharedStatus }: TripShareButtonProps) => {
     navigator.clipboard
       .writeText(shareUrl ?? '')
       .then(() => {
-        console.log('success');
+        setToastList((prevToastList) => [
+          ...prevToastList,
+          {
+            id: generateUniqueId(),
+            variant: 'default',
+            message: '공유 url이 클립보드에 복사되었습니다.',
+          },
+        ]);
       })
       .catch(() => {
-        console.log('error');
+        setToastList((prevToastList) => [
+          ...prevToastList,
+          {
+            id: generateUniqueId(),
+            variant: 'error',
+            message: '복사에 실패했습니다. 잠시 후 다시 시도해주세요.',
+          },
+        ]);
       });
   };
 
