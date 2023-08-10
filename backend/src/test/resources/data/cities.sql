1eae5c425aa80b291fbb6c9477732c6c4c4559d0
waterricecake <fruturum@nate.com>
2023-08-10 14:11:00 +0900

    feat: 통합테스트 sql 작성

---
+++ backend/src/test/resources/data/cities.sql
@@ -0,0 +1,8 @@
+INSERT INTO city(country, name, latitude, longitude, created_at, modified_at, status)
+VALUES ('미국', '켈리포니아', 37.9838096, 23.7275388, now(), now(), 'USABLE');
+INSERT INTO city(country, name, latitude, longitude, created_at, modified_at, status)
+VALUES ('일본', '도쿄', -33.9248685, 18.4240553, now(), now(), 'USABLE');
+INSERT INTO city(country, name, latitude, longitude, created_at, modified_at, status)
+VALUES ('중국', '베이징', 52.3675734, 4.9041389, now(), now(), 'USABLE');
+INSERT INTO city(country, name, latitude, longitude, created_at, modified_at, status)
+VALUES ('영국', '런던', 52.3675734, 4.9041389, now(), now(), 'USABLE');
