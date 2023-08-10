1d0195953f45889ff3f1ffb642da77de7985d3d7
waterricecake <fruturum@nate.com>
2023-08-10 16:20:16 +0900

    refactor: 대학교 카테고리 추가

--- backend/src/test/resources/data/categories.sql
+++ backend/src/test/resources/data/categories.sql
@@ -3,6 +3,8 @@ VALUES (100, 'food', '음식', now(), now(), 'USABLE');
 INSERT INTO category(id, eng_name, kor_name, created_at, modified_at, status)
 VALUES (200, 'culture', '문화', now(), now(), 'USABLE');
 INSERT INTO category(id, eng_name, kor_name, created_at, modified_at, status)
+VALUES (264, 'university', '대학교', now(), now(), 'USABLE');
+INSERT INTO category(id, eng_name, kor_name, created_at, modified_at, status)
 VALUES (400, 'accomodation', '숙박', now(), now(), 'USABLE');
 INSERT INTO category(id, eng_name, kor_name, created_at, modified_at, status)
 VALUES (500, 'transportation', '교통', now(), now(), 'USABLE');
