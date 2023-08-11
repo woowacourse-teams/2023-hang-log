a29a8cac0e9c64f23fff704e6b11c8f36c57794c
jjongwa <troas96@naver.com>
2023-08-11 22:09:04 +0900

    refactor: refreshToken 엔티티 구조 변경

--- backend/src/main/java/hanglog/member/domain/entity/RefreshToken.java
+++ backend/src/main/java/hanglog/member/domain/entity/RefreshToken.java
@@ -1,37 +1,26 @@
 package hanglog.member.domain.entity;
 
-import static jakarta.persistence.FetchType.LAZY;
-import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
-import hanglog.global.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = PROTECTED)
-public class RefreshToken extends BaseEntity {
+public class RefreshToken {
 
     @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private Long id;
-
-    @OneToOne(fetch = LAZY)
-    @JoinColumn(name = "member_id", nullable = false)
-    private Member member;
-
-    @Column(nullable = false)
     private String token;
 
-    public RefreshToken(final Member member, final String token) {
-        this.member = member;
+    @Column(nullable = false, unique = true)
+    private Long memberId;
+
+    public RefreshToken(final String token, final Long memberId) {
         this.token = token;
+        this.memberId = memberId;
     }
 }
