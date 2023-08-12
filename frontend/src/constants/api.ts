fda66a5bd27f651f1fe927b3c9a2cfab04553c5a
dladncks1217 <dlaxodud1217@gmail.com>
2023-08-12 20:24:52 +0900

    refactor: OAuth RedirectURI 잘못 매핑되는 문제 수정

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,7 +1,7 @@
 export const PROD = process.env.NODE_ENV === 'production';
 
 export const BASE_URL = PROD
-  ? `${window.location.protocol}//${process.env.PROD_BASE_URL}/api`
+  ? `${window.location.protocol}//${process.env.PROD_BASE_URL}`
   : 'http://localhost:3000';
 
 export const AXIOS_BASE_URL = PROD
