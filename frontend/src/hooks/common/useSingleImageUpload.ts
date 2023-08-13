4dab9cf7d915c8bf2e4073d2dd471fdd47150235
Ashley Heo <ashleysyheo@gmail.com>
2023-08-13 18:32:48 +0900

    refactor: useSingleImageUpload 커스텀 훅 생성

---
+++ frontend/src/hooks/common/useSingleImageUpload.ts
@@ -0,0 +1,51 @@
+import type { ChangeEvent } from 'react';
+import { useCallback, useState } from 'react';
+
+import { useImageMutation } from '@hooks/api/useImageMutation';
+
+interface UseImageUploadParams {
+  initialImageUrl: string | null;
+  maxUploadCount?: number;
+  onSuccess?: CallableFunction;
+}
+
+export const useSingleImageUpload = ({ initialImageUrl, onSuccess }: UseImageUploadParams) => {
+  const imageMutation = useImageMutation();
+
+  const [uploadedImageUrl, setUploadedImageUrl] = useState(initialImageUrl);
+
+  const handleImageUpload = useCallback(
+    async (event: ChangeEvent<HTMLInputElement>) => {
+      const imageFiles = event.target.files;
+
+      if (!imageFiles) return;
+
+      const imageUploadFormData = new FormData();
+
+      [...imageFiles].forEach((file) => {
+        imageUploadFormData.append('images', file);
+      });
+
+      imageMutation.mutate(
+        { images: imageUploadFormData },
+        {
+          onSuccess: ({ imageUrls }) => {
+            setUploadedImageUrl(imageUrls[0]);
+            onSuccess?.(imageUrls[0]);
+          },
+        }
+      );
+
+      // eslint-disable-next-line no-param-reassign
+      event.target.value = '';
+    },
+    [imageMutation, onSuccess]
+  );
+
+  const handleImageRemoval = useCallback(() => {
+    setUploadedImageUrl(null);
+    onSuccess?.(null);
+  }, [onSuccess]);
+
+  return { uploadedImageUrl, handleImageUpload, handleImageRemoval };
+};
