51d371bcc7b905ebabef667a30c2862dac773367
Ashley Heo <ashleysyheo@gmail.com>
2023-08-13 19:19:01 +0900

    refactor: 불필요한 레스트 파라미터 사용 제거

--- frontend/src/api/member/putUserInfo.ts
+++ frontend/src/api/member/putUserInfo.ts
@@ -5,5 +5,5 @@ import type { UserData } from '@type/member';
 import { END_POINTS } from '@constants/api';
 
 export const putUserInfo = (data: UserData) => {
-  return axiosInstance.put<UserData>(END_POINTS.MY_PAGE, { ...data });
+  return axiosInstance.put<UserData>(END_POINTS.MY_PAGE, data);
 };
