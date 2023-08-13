1fd011ca256cbd7503f578ff0cd5bc062e273ff4
LJW25 <dbjwlee@gmail.com>
2023-08-14 01:01:18 +0900

    test: 데이로그 조회 통합테스트 구현

---
+++ backend/src/test/java/hanglog/trip/integration/DayLogIntegrationTest.java
@@ -0,0 +1,101 @@
+package hanglog.trip.integration;
+
+import static hanglog.global.IntegrationFixture.EDINBURGH;
+import static hanglog.global.IntegrationFixture.END_DATE;
+import static hanglog.global.IntegrationFixture.LONDON;
+import static hanglog.global.IntegrationFixture.START_DATE;
+import static io.restassured.http.ContentType.JSON;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import hanglog.global.IntegrationTest;
+import hanglog.trip.dto.request.DayLogUpdateTitleRequest;
+import hanglog.trip.dto.request.ItemsOrdinalUpdateRequest;
+import hanglog.trip.dto.request.TripCreateRequest;
+import hanglog.trip.dto.response.DayLogResponse;
+import hanglog.trip.dto.response.TripDetailResponse;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+public class DayLogIntegrationTest extends IntegrationTest {
+
+    private Long tripId;
+    private Long dayLogId;
+
+    @BeforeEach
+    void setUp() {
+        final TripCreateRequest tripCreateRequest = new TripCreateRequest(
+                START_DATE,
+                END_DATE,
+                Arrays.asList(LONDON.getId(), EDINBURGH.getId())
+        );
+
+        final ExtractableResponse<Response> tripCreateResponse = TripIntegrationTest.requestCreateTrip(
+                tripCreateRequest);
+        tripId = Long.parseLong(parseUri(tripCreateResponse.header("Location")));
+
+        final ExtractableResponse<Response> tripGetResponse = TripIntegrationTest.requestGetTrips(tripId);
+        final TripDetailResponse tripDetailResponse = tripGetResponse.as(TripDetailResponse.class);
+        dayLogId = tripDetailResponse.getDayLogs().get(0).getId();
+    }
+
+    @DisplayName("데이로그를 조회한다.")
+    @Test
+    void getDayLog() {
+        // given
+        final DayLogResponse expected = new DayLogResponse(
+                dayLogId,
+                "",
+                1,
+                START_DATE,
+                new ArrayList<>()
+        );
+
+        // when
+        final ExtractableResponse<Response> response = requestGetDayLog(tripId, dayLogId);
+        final DayLogResponse result = response.as(DayLogResponse.class);
+
+        // then
+        assertSoftly(
+                softly -> {
+                    softly.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+                    softly.assertThat(result).usingRecursiveComparison()
+                            .isEqualTo(expected);
+                }
+        );
+    }
+
+    private ExtractableResponse<Response> requestGetDayLog(final Long tripId, final Long dayLogId) {
+        return RestAssured
+                .given().log().all()
+                .when().get("/trips/{tripId}/daylogs/{dayLogId}", tripId, dayLogId)
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> requestUpdateDayLogTitle(final DayLogUpdateTitleRequest request) {
+        return RestAssured
+                .given().log().all()
+                .contentType(JSON)
+                .body(request)
+                .when().patch("/trips/{tripId}/daylogs/{dayLogId}", tripId, dayLogId)
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> requestUpdateOrdinalOfItems(final ItemsOrdinalUpdateRequest request) {
+        return RestAssured
+                .given().log().all()
+                .contentType(JSON)
+                .body(request)
+                .when().patch("/trips/{tripId}/daylogs/{dayLogId}/order", tripId, dayLogId)
+                .then().log().all()
+                .extract();
+    }
+}
