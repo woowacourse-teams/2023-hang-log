34422bcdde19b406110769412b0d1d9ed04fba2b
mcodnjs <codnjs8989@gmail.com>
2023-08-14 18:09:31 +0900

    refactor: city sql 데이터 삭제

--- backend/src/test/resources/data/cities.sql
+++ backend/src/test/resources/data/cities.sql
@@ -4,5 +4,3 @@ INSERT INTO city(country, name, latitude, longitude, created_at, modified_at, st
 VALUES ('영국', '에든버러', 55.953252, -3.188267, now(), now(), 'USABLE');
 INSERT INTO city(country, name, latitude, longitude, created_at, modified_at, status)
 VALUES ('프랑스', '파리', 48.856614, 2.3522219, now(), now(), 'USABLE');
-INSERT INTO city(country, name, latitude, longitude, created_at, modified_at, status)
-VALUES ('도쿄', '일본', 35.6761919, 139.6503106, now(), now(), 'USABLE');
