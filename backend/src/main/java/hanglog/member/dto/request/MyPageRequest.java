8117e2a7e08b994da8be4bd85c91a72d58bcff16
jjongwa <troas96@naver.com>
2023-08-15 03:00:07 +0900

    refactor: 사용하지 않는 생성자 제거

--- backend/src/main/java/hanglog/member/dto/request/MyPageRequest.java
+++ backend/src/main/java/hanglog/member/dto/request/MyPageRequest.java
@@ -1,7 +1,5 @@
 package hanglog.member.dto.request;
 
-import hanglog.member.domain.Member;
-import hanglog.member.dto.response.MyPageResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -12,8 +10,4 @@ public class MyPageRequest {
     private final String nickname;
 
     private final String imageUrl;
-
-    public static MyPageResponse from(final Member member) {
-        return new MyPageResponse(member.getNickname(), member.getImageUrl());
-    }
 }
