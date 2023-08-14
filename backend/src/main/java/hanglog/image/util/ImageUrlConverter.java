8ff62bae68c0a2b881016ca805d120525b8b532b
hgo641 <hgo641@gmail.com>
2023-08-14 14:02:50 +0900

    refactor: 이미지 조회시 baseUrl 변경

--- backend/src/main/java/hanglog/image/util/ImageUrlConverter.java
+++ backend/src/main/java/hanglog/image/util/ImageUrlConverter.java
@@ -4,21 +4,30 @@ import static hanglog.global.exception.ExceptionCode.INVALID_IMAGE_URL;
 
 import hanglog.global.exception.BadRequestException;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ImageUrlConverter {
 
-    private static final String IMAGE_BASE_URL = "https://hanglog.com/img/";
     private static final String EMPTY_STRING = "";
     private static final int URL_INDEX = 1;
     private static final int VALID_PARSED_URL_SIZE = 2;
     private static final int EMPTY_STRING_INDEX = 0;
 
+    private static String imageBaseUrl;
+
+    @Value("${image.base-url}")
+    public void setImageBaseUrl(final String value) {
+        imageBaseUrl = value;
+    }
+
     public static String convertNameToUrl(final String name) {
-        return IMAGE_BASE_URL + name;
+        return imageBaseUrl + name;
     }
 
     public static String convertUrlToName(final String url) {
-        final List<String> parsedUrl = List.of(url.split(IMAGE_BASE_URL));
+        final List<String> parsedUrl = List.of(url.split(imageBaseUrl));
         validateImageUrlFormat(parsedUrl);
         return parsedUrl.get(URL_INDEX);
     }
