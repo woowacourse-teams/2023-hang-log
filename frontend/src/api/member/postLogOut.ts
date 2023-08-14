b5c03b50349acea71a0a455fd43b630ac6b5c2e1
dladncks1217 <dlaxodud1217@gmail.com>
2023-08-14 15:03:06 +0900

    refactor: 로그아웃 API 수정으로 인한 리팩토링

--- frontend/src/api/member/postLogOut.ts
+++
@@ -1,9 +0,0 @@
-import { axiosInstance } from '@api/axiosInstance';
-
-import type { TokenData } from '@type/member';
-
-import { END_POINTS } from '@constants/api';
-
-export const postLogout = (data: TokenData) => {
-  return axiosInstance.post(END_POINTS.LOGOUT, data);
-};
