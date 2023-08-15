53d16544bcb90513da45b92d7efd9505ce37c6d5
mcodnjs <codnjs8989@gmail.com>
2023-08-15 14:59:36 +0900

    refactor: BadRequestException 상속

--- backend/src/main/java/hanglog/global/exception/InvalidDomainException.java
+++ backend/src/main/java/hanglog/global/exception/InvalidDomainException.java
@@ -3,13 +3,9 @@ package hanglog.global.exception;
 import lombok.Getter;
 
 @Getter
-public class InvalidDomainException extends RuntimeException {
-
-    private final int code;
-    private final String message;
+public class InvalidDomainException extends BadRequestException {
 
     public InvalidDomainException(final ExceptionCode exceptionCode) {
-        this.code = exceptionCode.getCode();
-        this.message = exceptionCode.getMessage();
+        super(exceptionCode);
     }
 }
