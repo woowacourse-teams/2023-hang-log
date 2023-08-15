ab750942891f5ca72cadda2bcde6249ca75ee27e
jjongwa <troas96@naver.com>
2023-08-15 15:12:29 +0900

    refactor: requestMapping, final 적용

--- backend/src/main/java/hanglog/member/controller/MemberController.java
+++ backend/src/main/java/hanglog/member/controller/MemberController.java
@@ -9,24 +9,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("mypage")
 public class MemberController {
 
     public final MemberService memberService;
 
-    @GetMapping("/mypage")
+    @GetMapping
     public ResponseEntity<MyPageResponse> getMyInfo(@Auth final Long memberId) {
         final MyPageResponse myPageResponse = memberService.getMyPageInfo(memberId);
         return ResponseEntity.ok().body(myPageResponse);
     }
 
-    @PutMapping("/mypage")
+    @PutMapping
     public ResponseEntity<Void> updateMyInfo(
             @Auth final Long memberId,
-            @RequestBody MyPageRequest myPageRequest
+            @RequestBody final MyPageRequest myPageRequest
     ) {
         memberService.updateMyPageInfo(memberId, myPageRequest);
         return ResponseEntity.noContent().build();
