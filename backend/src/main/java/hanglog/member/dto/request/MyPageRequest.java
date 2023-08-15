00f7faba5b3f694f60997cbef6e8e3c198d7d45f
jjongwa <troas96@naver.com>
2023-08-15 19:12:06 +0900

    refactor: dto 제한 조건 추가

--- backend/src/main/java/hanglog/member/dto/request/MyPageRequest.java
+++ backend/src/main/java/hanglog/member/dto/request/MyPageRequest.java
@@ -1,5 +1,7 @@
 package hanglog.member.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,7 +9,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MyPageRequest {
 
+    @NotBlank(message = "닉네임은 공백이 될 수 없습니다.")
+    @Size(max = 15, message = "닉네임은 15자를 초과할 수 없습니다.")
     private final String nickname;
 
+    @NotBlank(message = "프로필 사진은 필수입니다.")
     private final String imageUrl;
 }
