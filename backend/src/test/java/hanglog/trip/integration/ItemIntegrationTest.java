fffd926b84feb1aac5e3ffdd0c3a8a9a8ee2e403
hgo641 <hgo641@gmail.com>
2023-08-16 16:45:21 +0900

    refactor: dto 생성 메서드 추가

--- backend/src/test/java/hanglog/trip/integration/ItemIntegrationTest.java
+++ backend/src/test/java/hanglog/trip/integration/ItemIntegrationTest.java
@@ -4,14 +4,19 @@ import static hanglog.global.IntegrationFixture.EDINBURGH;
 import static hanglog.global.IntegrationFixture.END_DATE;
 import static hanglog.global.IntegrationFixture.LONDON;
 import static hanglog.global.IntegrationFixture.START_DATE;
+import static hanglog.global.exception.ExceptionCode.NOT_FOUND_CATEGORY_ID;
 import static io.restassured.http.ContentType.JSON;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 
+import hanglog.category.domain.Category;
+import hanglog.category.domain.repository.CategoryRepository;
 import hanglog.category.dto.CategoryResponse;
+import hanglog.currency.domain.type.CurrencyType;
 import hanglog.expense.dto.response.ItemExpenseResponse;
 import hanglog.global.IntegrationTest;
+import hanglog.global.exception.BadRequestException;
 import hanglog.trip.dto.request.ExpenseRequest;
 import hanglog.trip.dto.request.ItemRequest;
 import hanglog.trip.dto.request.ItemUpdateRequest;
@@ -31,12 +36,16 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 public class ItemIntegrationTest extends IntegrationTest {
 
+    @Autowired
+    private CategoryRepository categoryRepository;
     private Long tripId;
     private Long dayLogId;
+
 
     @BeforeEach
     void setUp() {
@@ -60,16 +69,7 @@ public class ItemIntegrationTest extends IntegrationTest {
     @Test
     void createItem_NonSpot() {
         // when
-        final ItemRequest itemRequest = new ItemRequest(
-                false,
-                "non spot",
-                null,
-                null,
-                dayLogId,
-                Collections.emptyList(),
-                null,
-                null
-        );
+        final ItemRequest itemRequest = getNonSpotItemRequest();
 
         final ExtractableResponse<Response> response = requestCreateItem(tripId, itemRequest);
         final Long itemId = Long.parseLong(parseUri(response.header("Location")));
@@ -94,6 +94,7 @@ public class ItemIntegrationTest extends IntegrationTest {
                     softly.assertThat(response.header("Location")).isNotBlank();
                     softly.assertThat(itemResponses.get(0))
                             .usingRecursiveComparison()
+                            .ignoringFields("id", "expense.id", "place.id")
                             .isEqualTo(expectedItemResponse);
                 }
         );
@@ -103,29 +104,7 @@ public class ItemIntegrationTest extends IntegrationTest {
     @Test
     void createItem_Spot() {
         // when
-        final PlaceRequest placeRequest = new PlaceRequest(
-                "place name",
-                BigDecimal.valueOf(1.1),
-                BigDecimal.valueOf(2.2),
-                List.of("temp1", "culture", "temp2")
-        );
-
-        final ExpenseRequest expenseRequest = new ExpenseRequest(
-                "EUR",
-                BigDecimal.valueOf(10.5),
-                200L
-        );
-
-        final ItemRequest itemRequest = new ItemRequest(
-                true,
-                "spot",
-                5.0,
-                "memo",
-                dayLogId,
-                List.of("https://hanglog.com/img/test1.png", "https://hanglog.com/img/test2.png"),
-                placeRequest,
-                expenseRequest
-        );
+        final ItemRequest itemRequest = getSpotItemRequest();
 
         final ExtractableResponse<Response> response = requestCreateItem(tripId, itemRequest);
         final Long itemId = Long.parseLong(parseUri(response.header("Location")));
@@ -146,7 +125,7 @@ public class ItemIntegrationTest extends IntegrationTest {
 
         final ItemExpenseResponse expectedItemExpenseResponse = new ItemExpenseResponse(
                 1L,
-                "EUR",
+                "eur",
                 BigDecimal.valueOf(10.5).setScale(3),
                 expectedCategoryResponse
         );
@@ -170,14 +149,96 @@ public class ItemIntegrationTest extends IntegrationTest {
                     softly.assertThat(response.header("Location")).isNotBlank();
                     softly.assertThat(itemResponses.get(0))
                             .usingRecursiveComparison()
+                            .ignoringFields("id", "expense.id", "place.id")
                             .isEqualTo(expectedItemResponse);
                 }
         );
     }
 
-    @DisplayName("Item을 수정한다.")
+    @DisplayName("NonSpot인 아이템을 Spot으로 수정한다.")
     @Test
-    void updateItem() {
+    void updateItem_NonSpotToSpot() {
+        // when
+        final ItemRequest itemRequest = getSpotItemRequest();
+        final ExtractableResponse<Response> createResponse = requestCreateItem(tripId, itemRequest);
+        final Long itemId = Long.parseLong(parseUri(createResponse.header("Location")));
+
+        final ItemUpdateRequest itemUpdateRequest = new ItemUpdateRequest(
+                false,
+                "updated item",
+                null,
+                null,
+                dayLogId,
+                Collections.emptyList(),
+                false,
+                null,
+                null
+        );
+
+        final ExtractableResponse<Response> response = requestUpdateItem(tripId, itemId, itemUpdateRequest);
+        final List<ItemResponse> itemResponses = requestGetItems(tripId, dayLogId);
+
+        final ItemResponse expectedItemResponse = createMockIdResponseBy(1, itemUpdateRequest);
+
+        // then
+        assertSoftly(
+                softly -> {
+                    softly.assertThat(response.statusCode()).isEqualTo(NO_CONTENT.value());
+                    softly.assertThat(itemResponses.get(0))
+                            .usingRecursiveComparison()
+                            .ignoringFields("id", "expense.id", "place.id")
+                            .isEqualTo(expectedItemResponse);
+                }
+        );
+    }
+
+    @DisplayName("Item의 장소를 수정한다.")
+    @Test
+    void updateItem_changePlace() {
+        // when
+        final ItemRequest itemRequest = getSpotItemRequest();
+        final ExtractableResponse<Response> createResponse = requestCreateItem(tripId, itemRequest);
+        final Long itemId = Long.parseLong(parseUri(createResponse.header("Location")));
+
+        final PlaceRequest updatedPlaceRequest = new PlaceRequest(
+                "updated place",
+                BigDecimal.valueOf(100.1),
+                BigDecimal.valueOf(200.2),
+                List.of("food")
+        );
+
+        final ItemUpdateRequest itemUpdateRequest = new ItemUpdateRequest(
+                true,
+                "updated item",
+                4.5,
+                "updated memo",
+                dayLogId,
+                List.of("https://hanglog.com/img/test1.png", "https://hanglog.com/img/test2.png"),
+                true,
+                updatedPlaceRequest,
+                getExpenseRequest()
+        );
+
+        final ExtractableResponse<Response> response = requestUpdateItem(tripId, itemId, itemUpdateRequest);
+        final List<ItemResponse> itemResponses = requestGetItems(tripId, dayLogId);
+
+        final ItemResponse expectedItemResponse = createMockIdResponseBy(1, itemUpdateRequest);
+
+        // then
+        assertSoftly(
+                softly -> {
+                    softly.assertThat(response.statusCode()).isEqualTo(NO_CONTENT.value());
+                    softly.assertThat(itemResponses.get(0))
+                            .usingRecursiveComparison()
+                            .ignoringFields("id", "expense.id", "place.id")
+                            .isEqualTo(expectedItemResponse);
+                }
+        );
+    }
+
+    @DisplayName("Spot에서 NonSpot으로 수정한다.")
+    @Test
+    void updateItem_SpotToNonSpot() {
         // when
         final ItemRequest itemRequest = getNonSpotItemRequest();
         final ExtractableResponse<Response> createResponse = requestCreateItem(tripId, itemRequest);
@@ -198,37 +259,7 @@ public class ItemIntegrationTest extends IntegrationTest {
         final ExtractableResponse<Response> response = requestUpdateItem(tripId, itemId, itemUpdateRequest);
         final List<ItemResponse> itemResponses = requestGetItems(tripId, dayLogId);
 
-        final CategoryResponse expectedCategoryResponse = new CategoryResponse(
-                200L,
-                "문화"
-        );
-
-        final PlaceResponse expectedPlaceResponse = new PlaceResponse(
-                1L,
-                itemUpdateRequest.getPlace().getName(),
-                itemUpdateRequest.getPlace().getLatitude().setScale(13),
-                itemUpdateRequest.getPlace().getLongitude().setScale(13),
-                expectedCategoryResponse
-        );
-
-        final ItemExpenseResponse expectedItemExpenseResponse = new ItemExpenseResponse(
-                1L,
-                "EUR",
-                BigDecimal.valueOf(10.5).setScale(3),
-                expectedCategoryResponse
-        );
-
-        final ItemResponse expectedItemResponse = new ItemResponse(
-                itemId,
-                itemUpdateRequest.getItemType(),
-                itemUpdateRequest.getTitle(),
-                1,
-                itemUpdateRequest.getRating(),
-                itemUpdateRequest.getMemo(),
-                itemUpdateRequest.getImageUrls(),
-                expectedPlaceResponse,
-                expectedItemExpenseResponse
-        );
+        final ItemResponse expectedItemResponse = createMockIdResponseBy(1, itemUpdateRequest);
 
         // then
         assertSoftly(
@@ -236,6 +267,7 @@ public class ItemIntegrationTest extends IntegrationTest {
                     softly.assertThat(response.statusCode()).isEqualTo(NO_CONTENT.value());
                     softly.assertThat(itemResponses.get(0))
                             .usingRecursiveComparison()
+                            .ignoringFields("id", "expense.id", "place.id")
                             .isEqualTo(expectedItemResponse);
                 }
         );
@@ -343,6 +375,59 @@ public class ItemIntegrationTest extends IntegrationTest {
                 List.of("https://hanglog.com/img/test1.png", "https://hanglog.com/img/test2.png"),
                 getPlaceRequest(),
                 getExpenseRequest()
+        );
+    }
+
+    private CategoryResponse getMockIdCategoryResponseBy(final PlaceRequest placeRequest) {
+        final List<Category> categories = categoryRepository.findByEngNameIn(placeRequest.getApiCategory());
+        if (categories.isEmpty()) {
+            return CategoryResponse.of(categoryRepository.findCategoryETC());
+        }
+        return CategoryResponse.of(categories.get(0));
+    }
+
+    private PlaceResponse createMockIdPlaceResponseBy(final PlaceRequest placeRequest) {
+        if (placeRequest == null) {
+            return null;
+        }
+        return new PlaceResponse(
+                1L,
+                placeRequest.getName(),
+                placeRequest.getLatitude().setScale(13),
+                placeRequest.getLongitude().setScale(13),
+                getMockIdCategoryResponseBy(placeRequest)
+        );
+    }
+
+    private ItemExpenseResponse createMockIdExpenseResponseBy(final ExpenseRequest expenseRequest) {
+        if (expenseRequest == null) {
+            return null;
+        }
+        final CategoryResponse categoryResponse = CategoryResponse.of(
+                categoryRepository.findById(expenseRequest.getCategoryId())
+                        .orElseThrow(() -> new BadRequestException(NOT_FOUND_CATEGORY_ID)));
+
+        final String currency = CurrencyType.getMappedCurrencyType(expenseRequest.getCurrency()).getCode();
+
+        return new ItemExpenseResponse(
+                1L,
+                currency,
+                expenseRequest.getAmount().setScale(3),
+                categoryResponse
+        );
+    }
+
+    private ItemResponse createMockIdResponseBy(final Integer ordinal, final ItemUpdateRequest itemUpdateRequest) {
+        return new ItemResponse(
+                1L,
+                itemUpdateRequest.getItemType(),
+                itemUpdateRequest.getTitle(),
+                ordinal,
+                itemUpdateRequest.getRating(),
+                itemUpdateRequest.getMemo(),
+                itemUpdateRequest.getImageUrls(),
+                createMockIdPlaceResponseBy(itemUpdateRequest.getPlace()),
+                createMockIdExpenseResponseBy(itemUpdateRequest.getExpense())
         );
     }
 }
