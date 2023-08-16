5f4f2bc6e3641bb413908f5a0f8f76535c18d183
LJW25 <dbjwlee@gmail.com>
2023-08-16 15:49:30 +0900

    refactor: 쿼리 left join 추가

--- backend/src/main/java/hanglog/trip/domain/repository/TripRepository.java
+++ backend/src/main/java/hanglog/trip/domain/repository/TripRepository.java
@@ -5,13 +5,15 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TripRepository extends JpaRepository<Trip, Long> {
 
     boolean existsByMemberIdAndId(final Long memberId, final Long id);
 
-    @Query("SELECT trip FROM Trip trip JOIN FETCH trip.sharedTrip")
+    @Query("SELECT trip FROM Trip trip LEFT JOIN FETCH trip.sharedTrip")
     Optional<Trip> findTripById(final Long id);
 
-    List<Trip> findAllByMemberId(final Long memberId);
+    @Query("SELECT trip FROM Trip trip LEFT JOIN FETCH trip.sharedTrip WHERE trip.member.id = :memberId")
+    List<Trip> findAllByMemberId(@Param("memberId") final Long memberId);
 }
