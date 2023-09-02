540f4897ea96ab21bd6e9bbc6fa4888e9d8eb203
Ashley Heo <ashleysyheo@gmail.com>
2023-09-02 18:25:11 +0900

    refactor: 이미지 용량 10mb 이상으로 인한 에러 발생 시 상황에 맞는 에러 메세지 보여주기

--- frontend/src/hooks/api/useImageMutation.ts
+++ frontend/src/hooks/api/useImageMutation.ts
@@ -6,7 +6,7 @@ import { useTokenError } from '@hooks/member/useTokenError';
 import { postImage } from '@api/image/postImage';
 import type { ErrorResponseData } from '@api/interceptors';
 
-import { ERROR_CODE } from '@constants/api';
+import { ERROR_CODE, HTTP_STATUS_CODE } from '@constants/api';
 
 export const useImageMutation = () => {
   const { createToast } = useToast();
@@ -17,6 +17,17 @@ export const useImageMutation = () => {
     onError: (error: ErrorResponseData) => {
       if (error.code && error.code > ERROR_CODE.TOKEN_ERROR_RANGE) {
         handleTokenError();
+
+        return;
+      }
+
+      if (
+        (error.code && error.code === ERROR_CODE.LARGE_IMAGE_FILE) ||
+        error.statusCode === HTTP_STATUS_CODE.CONTENT_TOO_LARGE
+      ) {
+        createToast(
+          '이미지 용량이 커서 업로드에 실패했습니다. 10mb 이하의 파일을 업로드해 주세요.'
+        );
 
         return;
       }
