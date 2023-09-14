0c8b4d3870641789f28a8c667ed7a850df3ecfff
jjongwa <troas96@naver.com>
2023-09-15 00:10:56 +0900

    feat: N+1 detector 적용

---
+++ backend/src/main/java/hanglog/detector/ConnectionProxyHandler.java
@@ -0,0 +1,45 @@
+package hanglog.detector;
+
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import lombok.RequiredArgsConstructor;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.framework.ProxyFactory;
+
+@RequiredArgsConstructor
+public class ConnectionProxyHandler implements MethodInterceptor {
+
+    private static final String JDBC_PREPARE_STATEMENT_METHOD_NAME = "prepareStatement";
+
+    private final Object connection;
+    private final LoggingForm loggingForm;
+
+    @Nullable
+    @Override
+    public Object invoke(@Nonnull final MethodInvocation invocation) throws Throwable {
+        final Object result = invocation.proceed();
+
+        if (hasConnection(result) && hasPreparedStatementInvoked(invocation)) {
+            final ProxyFactory proxyFactory = new ProxyFactory(result);
+            proxyFactory.addAdvice(new PreparedStatementProxyHandler(loggingForm));
+            return proxyFactory.getProxy();
+        }
+
+        return result;
+    }
+
+    private boolean hasPreparedStatementInvoked(final MethodInvocation invocation) {
+        return invocation.getMethod().getName().equals(JDBC_PREPARE_STATEMENT_METHOD_NAME);
+    }
+
+    private boolean hasConnection(final Object result) {
+        return result != null;
+    }
+
+    public Object getProxy() {
+        final ProxyFactory proxyFactory = new ProxyFactory(connection);
+        proxyFactory.addAdvice(this);
+        return proxyFactory.getProxy();
+    }
+}
---
+++ backend/src/main/java/hanglog/detector/PreparedStatementProxyHandler.java
@@ -0,0 +1,38 @@
+package hanglog.detector;
+
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import java.lang.reflect.Method;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+@RequiredArgsConstructor
+public class PreparedStatementProxyHandler implements MethodInterceptor {
+
+    private static final List<String> JDBC_QUERY_METHOD =
+            List.of("executeQuery", "execute", "executeUpdate");
+
+    private final LoggingForm loggingForm;
+
+    @Nullable
+    @Override
+    public Object invoke(@Nonnull final MethodInvocation invocation) throws Throwable {
+
+        final Method method = invocation.getMethod();
+
+        if (JDBC_QUERY_METHOD.contains(method.getName())) {
+            final long startTime = System.currentTimeMillis();
+            final Object result = invocation.proceed();
+            final long endTime = System.currentTimeMillis();
+
+            loggingForm.addQueryTime(endTime - startTime);
+            loggingForm.queryCountUp();
+
+            return result;
+        }
+
+        return invocation.proceed();
+    }
+}
