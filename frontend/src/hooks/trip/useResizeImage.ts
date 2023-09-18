90415fa0887ceb5f90700f2b7c839860ef5fa101
Ashley Heo <ashleysyheo@gmail.com>
2023-09-18 16:37:44 +0900

    feat: 이미지 확대 모달 구현

--- frontend/src/hooks/trip/useResizeImage.ts
+++ frontend/src/hooks/trip/useResizeImage.ts
@@ -4,35 +4,13 @@ import { useMemo } from 'react';
 
 import { useRecoilValue } from 'recoil';
 
-interface useResizeImageParam {
-  width?: number;
-  height?: number;
-}
-
-interface mobileImageSizeType {
-  mobileImageSize: { width: number; height: number };
-  modalImageSize: { width: number; height: number };
-}
-
-const useResizeImage = ({ width = 0, height = 0 }: useResizeImageParam): mobileImageSizeType => {
+const useResizeImage = () => {
   const viewportWidth = useRecoilValue(viewportWidthState);
 
-  const mobileImageWidth = useMemo(() => viewportWidth - 48, [viewportWidth]);
-  const mobileImageHeight = useMemo(() => (mobileImageWidth / 4.5) * 3, [mobileImageWidth]);
+  const width = useMemo(() => viewportWidth - 48, [viewportWidth]);
+  const height = useMemo(() => (width / 4.5) * 3, [width]);
 
-  const modalImageWidth = useMemo(() => width * 2, [width]);
-  const modalImageHeight = useMemo(() => height * 2, [height]);
-
-  return {
-    mobileImageSize: {
-      height: mobileImageHeight,
-      width: mobileImageWidth,
-    },
-    modalImageSize: {
-      height: modalImageHeight,
-      width: modalImageWidth,
-    },
-  };
+  return { width, height };
 };
 
 export default useResizeImage;
