789c776d8f8bc63b6d4ea8b763c2cf00c814aa24
waterricecake <fruturum@nate.com>
2023-09-19 10:51:20 +0900

    feat: TripDetailResponse에 publishedStatus 추가

--- backend/src/main/java/hanglog/trip/dto/response/TripDetailResponse.java
+++ backend/src/main/java/hanglog/trip/dto/response/TripDetailResponse.java
@@ -3,9 +3,10 @@ package hanglog.trip.dto.response;
 import static hanglog.image.util.ImageUrlConverter.convertNameToUrl;
 import static lombok.AccessLevel.PRIVATE;
 
-import hanglog.city.dto.response.CityWithPositionResponse;
 import hanglog.city.domain.City;
+import hanglog.city.dto.response.CityWithPositionResponse;
 import hanglog.trip.domain.Trip;
+import hanglog.trip.domain.type.PublishedStatusType;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.Getter;
@@ -24,6 +25,7 @@ public class TripDetailResponse {
     private final String imageUrl;
     private final String sharedCode;
     private final List<DayLogResponse> dayLogs;
+    private final Boolean isPublished;
 
     public static TripDetailResponse of(final Trip trip, final List<City> cities) {
         final List<DayLogResponse> dayLogResponses = trip.getDayLogs().stream()
@@ -36,6 +38,8 @@ public class TripDetailResponse {
 
         final String sharedCode = trip.getSharedCode().orElse(null);
 
+        final Boolean isPublished = PublishedStatusType.PUBLISHED.equals(trip.getPublishedStatus());
+
         return new TripDetailResponse(
                 trip.getId(),
                 cityWithPositionResponses,
@@ -45,7 +49,8 @@ public class TripDetailResponse {
                 trip.getDescription(),
                 convertNameToUrl(trip.getImageName()),
                 sharedCode,
-                dayLogResponses
+                dayLogResponses,
+                isPublished
         );
     }
 }
