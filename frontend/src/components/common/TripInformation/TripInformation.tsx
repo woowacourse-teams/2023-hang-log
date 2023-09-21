5fa15767cb9eeca45a900dd9debf6022b2bead72
dahyeee <06robin11@gmail.com>
2023-09-21 20:50:45 +0900

    fix:경비공유페이지 버그 수정 => 잘못된 id전달로 인한 오류

--- frontend/src/components/common/TripInformation/TripInformation.tsx
+++ frontend/src/components/common/TripInformation/TripInformation.tsx
@@ -65,13 +65,9 @@ const TripInformation = ({ isEditable = true, isShared = false, tripId }: TripIn
         </Box>
         <Box css={buttonContainerStyling}>
           {isEditable ? (
-            <TripEditButtons tripId={String(tripData.id)} openEditModal={openEditModal} />
+            <TripEditButtons tripId={tripId} openEditModal={openEditModal} />
           ) : (
-            <TripButtons
-              tripId={String(tripData.id)}
-              sharedCode={tripData.sharedCode}
-              isShared={isShared}
-            />
+            <TripButtons tripId={tripId} sharedCode={tripData.sharedCode} isShared={isShared} />
           )}
         </Box>
       </header>
