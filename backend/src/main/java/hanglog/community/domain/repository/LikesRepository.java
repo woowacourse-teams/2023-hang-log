ed4c0215934fe05e7f0e7d9609e225fabf35b8b7
jjongwa <troas96@naver.com>
2023-09-26 14:52:02 +0900

    feat: LikesRepository 메서드 추가
    countByMemberIdAndTripIds: tripId 리스트와 memberId를 매개변수로 받았을 때 각각 trip의 좋아요 개수와 해당 멤버의 좋아요 반영 여부 리스트를 반환하는 메서드
    
    Co-authored-by: mcodnjs <codnjs8989@gmail.com>

--- backend/src/main/java/hanglog/community/domain/repository/LikesRepository.java
+++ backend/src/main/java/hanglog/community/domain/repository/LikesRepository.java
@@ -1,7 +1,10 @@
 package hanglog.community.domain.repository;
 
 import hanglog.community.domain.Likes;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LikesRepository extends JpaRepository<Likes, Long> {
 
@@ -10,4 +13,12 @@ public interface LikesRepository extends JpaRepository<Likes, Long> {
     void deleteByMemberIdAndTripId(final Long memberId, final Long tripId);
 
     Long countLikesByTripId(final Long tripId);
+
+    @Query("SELECT l.tripId, COUNT(l) AS row_count, " +
+            "CASE WHEN MAX(CASE WHEN l.memberId = :memberId THEN 1 ELSE 0 END) = 1 THEN TRUE ELSE FALSE END AS" +
+            " member_likes_trip " +
+            "FROM Likes l " +
+            "WHERE l.tripId IN :tripIds " +
+            "GROUP BY l.tripId")
+    List<Object[]> countByMemberIdAndTripIds(@Param("memberId") Long memberId, @Param("tripIds") List<Long> tripIds);
 }
