90bf34660126b46e07b4b7ce86b2979b0eb26d2c
LJW25 <dbjwlee@gmail.com>
2023-10-06 13:44:34 +0900

    refactor: 로그아웃 로직 변경

--- backend/src/main/java/hanglog/auth/domain/repository/RefreshTokenRepository.java
+++ backend/src/main/java/hanglog/auth/domain/repository/RefreshTokenRepository.java
@@ -4,10 +4,10 @@ import hanglog.auth.domain.RefreshToken;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
 
     Optional<RefreshToken> findByToken(final String token);
-    
+
     boolean existsByToken(final String token);
 
     void deleteByMemberId(final Long memberId);
