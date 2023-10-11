c2e47649530e84273b02f65132c215026d7e73d2
LJW25 <dbjwlee@gmail.com>
2023-10-11 22:43:28 +0900

    refactor: 아이템 삭제 쿼리 개선

--- backend/src/main/java/hanglog/trip/domain/repository/ItemRepository.java
+++ backend/src/main/java/hanglog/trip/domain/repository/ItemRepository.java
@@ -2,12 +2,33 @@ package hanglog.trip.domain.repository;
 
 import hanglog.trip.domain.Item;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    @Query("""
+            SELECT item
+            FROM Item item
+            LEFT JOIN FETCH item.images images
+            LEFT JOIN FETCH item.expense expense
+            LEFT JOIN FETCH item.place place
+            LEFT JOIN FETCH expense.category expense_category
+            LEFT JOIN FETCH place.category place_category
+            WHERE item.id = :itemId
+            """)
+    Optional<Item> findById(@Param("itemId") final Long itemId);
+
+    @Modifying
+    @Query("""
+            UPDATE Item item
+            SET item.status = 'DELETED'
+            WHERE item.id = :itemId
+            """)
+    void deleteById(@Param("itemId") final Long itemId);
 
     @Modifying
     @Query("""
