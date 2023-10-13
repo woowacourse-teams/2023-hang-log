1aad756ff772e474680fafd5b4c83ff1d47c89e0
mcodnjs <codnjs8989@gmail.com>
2023-10-13 10:32:24 +0900

    style: 쿼리 포맷 수정

--- backend/src/main/java/hanglog/trip/domain/repository/TripCityRepository.java
+++ backend/src/main/java/hanglog/trip/domain/repository/TripCityRepository.java
@@ -21,8 +21,9 @@ public interface TripCityRepository extends JpaRepository<TripCity, Long> {
     void deleteAllByTripId(@Param("tripId") final Long tripId);
 
     @Query("""
-            SELECT new hanglog.community.dto.CityInfoByTrip
-            (tc.trip.id, tc.city) FROM TripCity tc WHERE tc.trip.id IN :tripIds
+            SELECT new hanglog.community.dto.CityInfoByTrip (tc.trip.id, tc.city) 
+            FROM TripCity tc 
+            WHERE tc.trip.id IN :tripIds
             """)
     List<CityInfoByTrip> findTripIdAndCitiesByTripIds(@Param("tripIds") final List<Long> tripIds);
 }
