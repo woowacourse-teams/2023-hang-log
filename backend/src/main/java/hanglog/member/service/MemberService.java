3db499b1911536160e2dffdf87fe915ee767a78f
jjongwa <troas96@naver.com>
2023-10-17 11:15:21 +0900

    feat: 마이페이지 프로필 사진 변경 시 S3에 존재하는 기존 파일 삭제 기능 추가

--- backend/src/main/java/hanglog/member/service/MemberService.java
+++ backend/src/main/java/hanglog/member/service/MemberService.java
@@ -1,14 +1,19 @@
 package hanglog.member.service;
 
 import static hanglog.global.exception.ExceptionCode.DUPLICATED_MEMBER_NICKNAME;
+import static hanglog.global.exception.ExceptionCode.FAIL_IMAGE_NAME_HASH;
 import static hanglog.global.exception.ExceptionCode.NOT_FOUND_MEMBER_ID;
 
 import hanglog.global.exception.BadRequestException;
+import hanglog.image.domain.S3ImageEvent;
 import hanglog.member.domain.Member;
 import hanglog.member.domain.repository.MemberRepository;
 import hanglog.member.dto.request.MyPageRequest;
 import hanglog.member.dto.response.MyPageResponse;
+import java.net.MalformedURLException;
+import java.net.URL;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,7 +22,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class MemberService {
 
+    private static final String KAKAO_HOST = "k.kakaocdn.net";
+    private static final String GOOGLE_HOST = "lh3.googleusercontent.com";
+
     private final MemberRepository memberRepository;
+    private final ApplicationEventPublisher publisher;
 
     @Transactional(readOnly = true)
     public MyPageResponse getMyPageInfo(final Long memberId) {
@@ -33,19 +42,35 @@ public class MemberService {
         if (member.isNicknameChanged(myPageRequest.getNickname())) {
             checkDuplicatedNickname(myPageRequest.getNickname());
         }
-
         final Member updateMember = new Member(
                 memberId,
                 member.getSocialLoginId(),
                 myPageRequest.getNickname(),
                 myPageRequest.getImageUrl()
         );
+        deleteBeforeImage(member.getImageUrl(), updateMember.getImageUrl());
         memberRepository.save(updateMember);
     }
 
     private void checkDuplicatedNickname(final String nickname) {
-        if(memberRepository.existsByNickname(nickname)) {
+        if (memberRepository.existsByNickname(nickname)) {
             throw new BadRequestException(DUPLICATED_MEMBER_NICKNAME);
         }
+    }
+
+    private void deleteBeforeImage(final String beforeUrl, final String updatedUrl) {
+        if (beforeUrl.equals(updatedUrl)) {
+            return;
+        }
+        try {
+            final URL targetUrl = new URL(beforeUrl);
+            if (targetUrl.getHost().equals(KAKAO_HOST) || targetUrl.getHost().equals(GOOGLE_HOST)) {
+                return;
+            }
+        } catch (MalformedURLException e) {
+            throw new BadRequestException(FAIL_IMAGE_NAME_HASH);
+        }
+        final String targetName = beforeUrl.substring(beforeUrl.lastIndexOf("/") + 1);
+        publisher.publishEvent(new S3ImageEvent(targetName));
     }
 }
