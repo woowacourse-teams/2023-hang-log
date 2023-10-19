0d05b9e8471d65de753181a946b721881fc66678
임우찬 <dlaxodud1217@naver.com>
2023-10-19 15:54:15 +0900

    trip 정렬 기준 수정, 하트 svg 수정, 여행 삭제 후 redirect 경로 수정 (#730)

--- frontend/src/utils/sort.ts
+++ frontend/src/utils/sort.ts
@@ -4,5 +4,5 @@ export const sortByStartDate = (a: TripsData, b: TripsData) => {
   const dateA = new Date(a.startDate);
   const dateB = new Date(b.startDate);
 
-  return dateA.getTime() - dateB.getTime();
+  return dateB.getTime() - dateA.getTime();
 };
