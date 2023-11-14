ce24ac9c72773692ad7b760a069c4d20938ff34c
Chaewon Moon <64852591+mcodnjs@users.noreply.github.com>
2023-11-14 19:36:27 +0900

    동기, 비동기 예외 핸들링 시 MDC를 활용하여 슬랙 예외 알림 (#749)
    * feat: 비동기 예외 처리
    
    * feat: mdc 설정 추가
    
    * feat: SlackNotification AOP 구현
    
    * feat: Slack 알림 Logback으로 구현
    
    * chore: 불필요한 출력문 제거
    
    * refactor: SlackMessageSender 삭제
    
    * refactor: slack api 의존성 제거
    
    * refactor: 슬랙 로깅 profile 변경
    
    * refactor: slack appender 최신 버전으로 변경
    
    * feat: slack webhook url 설정 추가

--- backend/src/main/java/hanglog/global/filter/MdcLoggingFilter.java
+++ backend/src/main/java/hanglog/global/filter/MdcLoggingFilter.java
@@ -1,11 +1,19 @@
 package hanglog.global.filter;
 
+import static hanglog.global.filter.MdcKey.REQUEST_ID;
+import static hanglog.global.filter.MdcKey.REQUEST_IP;
+import static hanglog.global.filter.MdcKey.REQUEST_METHOD;
+import static hanglog.global.filter.MdcKey.REQUEST_TIME;
+import static hanglog.global.filter.MdcKey.REQUEST_URI;
+
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import org.slf4j.MDC;
 import org.springframework.core.Ordered;
@@ -16,16 +24,22 @@ import org.springframework.stereotype.Component;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class MdcLoggingFilter implements Filter {
 
-    private static final String REQUEST_ID = "REQUEST_ID";
-
     @Override
     public void doFilter(
             final ServletRequest request,
             final ServletResponse response,
             final FilterChain chain
     ) throws IOException, ServletException {
-        MDC.put(REQUEST_ID, UUID.randomUUID().toString());
+        setMdc((HttpServletRequest) request);
         chain.doFilter(request, response);
         MDC.clear();
+    }
+
+    private void setMdc(final HttpServletRequest request) {
+        MDC.put(REQUEST_ID.name(), UUID.randomUUID().toString());
+        MDC.put(REQUEST_METHOD.name(), request.getMethod());
+        MDC.put(REQUEST_URI.name(), request.getRequestURI());
+        MDC.put(REQUEST_TIME.name(), LocalDateTime.now().toString());
+        MDC.put(REQUEST_IP.name(), request.getRemoteAddr());
     }
 }
