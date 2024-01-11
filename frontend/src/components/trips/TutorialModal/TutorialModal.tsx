7d24bc3463716c81002deaf21dbcaef093922c7b
dladncks1217 <dlaxodud1217@gmail.com>
2024-01-11 14:36:29 +0900

    refactor: 디자인시스템 컴포넌트 개선에 의한 코드 수정

--- frontend/src/components/trips/TutorialModal/TutorialModal.tsx
+++ frontend/src/components/trips/TutorialModal/TutorialModal.tsx
@@ -2,7 +2,13 @@ import { useEffect } from 'react';
 
 import { useRecoilValue } from 'recoil';
 
-import { SVGCarouselModal, useOverlay } from 'hang-log-design-system';
+import { Button, GeneralCarousel, Modal, useOverlay } from 'hang-log-design-system';
+
+import {
+  ItemStyling,
+  buttonStyling,
+  modalStyling,
+} from '@components/trips/TutorialModal/TutorialModal.style';
 
 import { mediaQueryMobileState } from '@store/mediaQuery';
 
@@ -14,24 +20,37 @@ import Tutorial4SVG from '@assets/svg/tutorial4.svg';
 const TutorialModal = () => {
   const { isOpen: isTutorialOpen, open: openTutorial, close: closeTutorial } = useOverlay();
   const isMobile = useRecoilValue(mediaQueryMobileState);
+  const carouselImages = [Tutorial1SVG, Tutorial2SVG, Tutorial3SVG, Tutorial4SVG];
 
   useEffect(() => {
     openTutorial();
   }, [openTutorial]);
 
   return (
-    <SVGCarouselModal
-      modalWidth={isMobile ? window.innerWidth - 32 - 48 : 385}
-      modalHeight={isMobile ? window.innerWidth : 412}
-      isOpen={isTutorialOpen}
-      closeModal={closeTutorial}
-      carouselWidth={isMobile ? window.innerWidth - 32 - 48 : 385}
-      carouselHeight={isMobile ? window.innerWidth : 412}
-      carouselImages={[Tutorial1SVG, Tutorial2SVG, Tutorial3SVG, Tutorial4SVG]}
-      showArrows
-      showDots
-      buttonGap={isMobile ? 0 : 48}
-    />
+    <Modal css={modalStyling} isOpen={isTutorialOpen} closeModal={closeTutorial}>
+      <GeneralCarousel
+        showNavigationOnHover={false}
+        width={isMobile ? window.innerWidth - 32 - 48 : 385}
+        height={isMobile ? window.innerWidth : 412}
+        length={4}
+      >
+        {carouselImages.map((Svg, index) => (
+          <GeneralCarousel.Item index={index} key={crypto.randomUUID()}>
+            <div
+              css={ItemStyling(
+                isMobile ? window.innerWidth - 32 - 48 : 385,
+                isMobile ? window.innerWidth : 412
+              )}
+            >
+              <Svg />
+            </div>
+          </GeneralCarousel.Item>
+        ))}
+      </GeneralCarousel>
+      <Button type="button" variant="primary" css={buttonStyling(isMobile)} onClick={closeTutorial}>
+        닫기
+      </Button>
+    </Modal>
   );
 };
 
