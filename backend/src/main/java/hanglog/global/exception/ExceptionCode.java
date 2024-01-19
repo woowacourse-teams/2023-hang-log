9341f48529fa60bea3e0d633e969ff810348e856
LJW25 <dbjwlee@gmail.com>
2024-01-19 16:28:33 +0900

    feat: AdminException 추가

--- backend/src/main/java/hanglog/global/exception/ExceptionCode.java
+++ backend/src/main/java/hanglog/global/exception/ExceptionCode.java
@@ -56,6 +56,15 @@ public enum ExceptionCode {
     INVALID_SHARE_CODE(7002, "공유가 허용되지 않은 코드입니다."),
     FAIL_SHARE_CODE_HASH(7101, "공유 코드를 해싱하는 데 실패했습니다."),
 
+    INVALID_USER_NAME(8001, "존재하지 않는 사용자입니다."),
+    INVALID_PASSWORD(8002, "비밀번호가 일치하지 않습니다."),
+    NULL_ADMIN_AUTHORITY(8101, "잘못된 관리자 권한입니다."),
+    DUPLICATED_ADMIN_USERNAME(8102, "중복된 사용자 이름입니다."),
+    NOT_FOUND_ADMIN_ID(8103, "요청한 ID에 해당하는 관리자를 찾을 수 없습니다."),
+    INVALID_CURRENT_PASSWORD(8104, "현재 사용중인 비밀번호가 일치하지 않습니다."),
+
+    INVALID_ADMIN_AUTHORITY(8201, "해당 관리자 기능에 대한 접근 권한이 없습니다."),
+
     INVALID_AUTHORIZATION_CODE(9001, "유효하지 않은 인증 코드입니다."),
     NOT_SUPPORTED_OAUTH_SERVICE(9002, "해당 OAuth 서비스는 제공하지 않습니다."),
     FAIL_TO_CONVERT_URL_PARAMETER(9003, "Url Parameter 변환 중 오류가 발생했습니다."),
