e8a0e4b4adfcbf805385fb304f1a9cd72cd88ad6
LJW25 <dbjwlee@gmail.com>
2024-01-19 16:29:00 +0900

    feat: AdminMember 구현

---
+++ backend/src/main/java/hanglog/admin/domain/AdminMember.java
@@ -0,0 +1,70 @@
+package hanglog.admin.domain;
+
+import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import hanglog.admin.domain.type.AdminType;
+import hanglog.member.domain.MemberState;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@SQLDelete(sql = "UPDATE admin_member SET status = 'DELETED' WHERE id = ?")
+@Where(clause = "status = 'ACTIVE'")
+public class AdminMember {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String userName;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(nullable = false)
+    private LocalDateTime lastLoginDate;
+
+    @Column(nullable = false)
+    @Enumerated(value = STRING)
+    private AdminType adminType;
+
+    @Enumerated(value = STRING)
+    private MemberState status;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    public AdminMember(final Long id, final String userName, final String password, final AdminType adminType) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.lastLoginDate = LocalDateTime.now();
+        this.adminType = adminType;
+        this.status = MemberState.ACTIVE;
+        this.createdAt = LocalDateTime.now();
+        this.modifiedAt = LocalDateTime.now();
+    }
+
+    public AdminMember(final String userName, final String password, final AdminType adminType) {
+        this(null, userName, password, adminType);
+    }
+}
