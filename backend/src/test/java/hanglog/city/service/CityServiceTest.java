540fd63bd1da0b02f16fb09a7e200f1c2846554c
LJW25 <dbjwlee@gmail.com>
2024-01-20 15:37:18 +0900

    test: city 추가 테스트 작성

--- backend/src/test/java/hanglog/city/service/CityServiceTest.java
+++ backend/src/test/java/hanglog/city/service/CityServiceTest.java
@@ -3,11 +3,17 @@ package hanglog.city.service;
 import static hanglog.trip.fixture.CityFixture.LONDON;
 import static hanglog.trip.fixture.CityFixture.PARIS;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
+import hanglog.city.domain.City;
 import hanglog.city.domain.repository.CityRepository;
+import hanglog.city.dto.request.CityRequest;
 import hanglog.city.dto.response.CityDetailResponse;
 import hanglog.city.dto.response.CityResponse;
+import hanglog.global.exception.InvalidDomainException;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -55,5 +61,44 @@ class CityServiceTest {
         // then
         assertThat(actual).usingRecursiveComparison()
                 .isEqualTo(List.of(CityDetailResponse.of(PARIS), CityDetailResponse.of(LONDON)));
+    }
+
+    @DisplayName("새로운 도시를 추가한다.")
+    @Test
+    void save() {
+        // given
+        final CityRequest cityRequest = new CityRequest(
+                PARIS.getName(),
+                PARIS.getCountry(),
+                PARIS.getLatitude(),
+                PARIS.getLongitude()
+        );
+
+        given(cityRepository.existsByNameAndCountry(anyString(), anyString())).willReturn(false);
+        given(cityRepository.save(any(City.class))).willReturn(PARIS);
+
+        // when
+        final Long actualId = cityService.save(cityRequest);
+
+        // then
+        assertThat(actualId).isEqualTo(PARIS.getId());
+    }
+
+    @DisplayName("도시와 나라가 중복되면 도시를 추가할 수 없다.")
+    @Test
+    void save_duplicateFail() {
+        // given
+        final CityRequest cityRequest = new CityRequest(
+                PARIS.getName(),
+                PARIS.getCountry(),
+                PARIS.getLatitude(),
+                PARIS.getLongitude()
+        );
+
+        given(cityRepository.existsByNameAndCountry(anyString(), anyString())).willReturn(true);
+
+        // when
+        assertThatThrownBy(() -> cityService.save(cityRequest))
+                .isInstanceOf(InvalidDomainException.class);
     }
 }
