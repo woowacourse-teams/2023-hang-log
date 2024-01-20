02d375d607c7149f4be4157fb90465254e391a5b
LJW25 <dbjwlee@gmail.com>
2024-01-20 18:51:20 +0900

    feat: 카테고리 추가 API 구현

--- backend/src/main/java/hanglog/admin/presentation/AdminCategoryController.java
+++ backend/src/main/java/hanglog/admin/presentation/AdminCategoryController.java
@@ -4,12 +4,17 @@ package hanglog.admin.presentation;
 import hanglog.auth.AdminAuth;
 import hanglog.auth.AdminOnly;
 import hanglog.auth.domain.Accessor;
+import hanglog.category.dto.request.CategoryRequest;
 import hanglog.category.dto.response.CategoryDetailResponse;
 import hanglog.category.service.CategoryService;
+import jakarta.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +32,15 @@ public class AdminCategoryController {
     ) {
         final List<CategoryDetailResponse> categoriesDetail = categoryService.getAllCategoriesDetail();
         return ResponseEntity.ok(categoriesDetail);
+    }
+
+    @PostMapping
+    @AdminOnly
+    public ResponseEntity<Void> createCategory(
+            @AdminAuth final Accessor accessor,
+            @RequestBody @Valid final CategoryRequest categoryRequest
+    ) {
+        final Long categoryId = categoryService.save(categoryRequest);
+        return ResponseEntity.created(URI.create("/admin/categories/" + categoryId)).build();
     }
 }
