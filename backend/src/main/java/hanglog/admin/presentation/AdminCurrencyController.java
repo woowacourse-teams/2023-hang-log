cd51552112c79c44d5718dbac6a1602da749f173
LJW25 <dbjwlee@gmail.com>
2024-01-20 21:03:32 +0900

    feat: 환율 조회 Api 구현

---
+++ backend/src/main/java/hanglog/admin/presentation/AdminCurrencyController.java
@@ -0,0 +1,35 @@
+package hanglog.admin.presentation;
+
+
+import static org.springframework.data.domain.Sort.Direction.DESC;
+
+import hanglog.auth.AdminAuth;
+import hanglog.auth.AdminOnly;
+import hanglog.auth.domain.Accessor;
+import hanglog.currency.dto.CurrencyListResponse;
+import hanglog.currency.service.CurrencyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/currencies")
+public class AdminCurrencyController {
+
+    private final CurrencyService currencyService;
+
+    @GetMapping
+    @AdminOnly
+    public ResponseEntity<CurrencyListResponse> getCurrencies(
+            @AdminAuth final Accessor accessor,
+            @PageableDefault(sort = "date", direction = DESC) final Pageable pageable
+    ) {
+        final CurrencyListResponse currencies = currencyService.getCurrenciesByPage(pageable);
+        return ResponseEntity.ok().body(currencies);
+    }
+}
