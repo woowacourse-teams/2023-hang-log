238889d9216326e06245ab70bbb39bafb81fa6a2
LJW25 <dbjwlee@gmail.com>
2024-01-20 21:52:55 +0900

    feat: 환율 생성 API 구현

--- backend/src/main/java/hanglog/admin/presentation/AdminCurrencyController.java
+++ backend/src/main/java/hanglog/admin/presentation/AdminCurrencyController.java
@@ -6,13 +6,18 @@ import static org.springframework.data.domain.Sort.Direction.DESC;
 import hanglog.auth.AdminAuth;
 import hanglog.auth.AdminOnly;
 import hanglog.auth.domain.Accessor;
+import hanglog.currency.dto.request.CurrencyRequest;
 import hanglog.currency.dto.response.CurrencyListResponse;
 import hanglog.currency.service.CurrencyService;
+import jakarta.validation.Valid;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +36,15 @@ public class AdminCurrencyController {
     ) {
         final CurrencyListResponse currencies = currencyService.getCurrenciesByPage(pageable);
         return ResponseEntity.ok().body(currencies);
+    }
+
+    @PostMapping
+    @AdminOnly
+    public ResponseEntity<Void> createCurrency(
+            @AdminAuth final Accessor accessor,
+            @RequestBody @Valid final CurrencyRequest currencyRequest
+    ) {
+        final Long currencyId = currencyService.save(currencyRequest);
+        return ResponseEntity.created(URI.create("/admin/currencies/" + currencyId)).build();
     }
 }
