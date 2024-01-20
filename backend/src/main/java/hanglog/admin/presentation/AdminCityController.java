b44580fb72e5ecf093b7c939a69e0c57730fb8e6
LJW25 <dbjwlee@gmail.com>
2024-01-20 16:59:32 +0900

    feat: 도시 수정 API 구현

--- backend/src/main/java/hanglog/admin/presentation/AdminCityController.java
+++ backend/src/main/java/hanglog/admin/presentation/AdminCityController.java
@@ -13,7 +13,9 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +44,16 @@ public class AdminCityController {
     ) {
         final Long cityId = cityService.save(cityRequest);
         return ResponseEntity.created(URI.create("/admin/cities/" + cityId)).build();
+    }
+
+    @PutMapping("/{cityId}")
+    @AdminOnly
+    public ResponseEntity<Void> updateCity(
+            @AdminAuth final Accessor accessor,
+            @PathVariable final Long cityId,
+            @RequestBody @Valid final CityRequest cityRequest
+    ) {
+        cityService.update(cityId, cityRequest);
+        return ResponseEntity.noContent().build();
     }
 }
