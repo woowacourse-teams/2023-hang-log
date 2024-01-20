a696b33cffa3dc2999db70a73869809e0a2fc33c
LJW25 <dbjwlee@gmail.com>
2024-01-20 16:40:25 +0900

    feat: 도시 추가 API 구현

--- backend/src/main/java/hanglog/admin/presentation/AdminCityController.java
+++ backend/src/main/java/hanglog/admin/presentation/AdminCityController.java
@@ -4,12 +4,17 @@ package hanglog.admin.presentation;
 import hanglog.auth.AdminAuth;
 import hanglog.auth.AdminOnly;
 import hanglog.auth.domain.Accessor;
+import hanglog.city.dto.request.CityRequest;
 import hanglog.city.dto.response.CityDetailResponse;
 import hanglog.city.service.CityService;
+import jakarta.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +32,15 @@ public class AdminCityController {
     ) {
         final List<CityDetailResponse> citiesDetail = cityService.getAllCitiesDetail();
         return ResponseEntity.ok(citiesDetail);
+    }
+
+    @PostMapping
+    @AdminOnly
+    public ResponseEntity<Void> createCity(
+            @AdminAuth final Accessor accessor,
+            @RequestBody @Valid final CityRequest cityRequest
+    ) {
+        final Long cityId = cityService.save(cityRequest);
+        return ResponseEntity.created(URI.create("/admin/cities" + cityId)).build();
     }
 }
