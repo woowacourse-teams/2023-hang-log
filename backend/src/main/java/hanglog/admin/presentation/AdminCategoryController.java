3ae8de857b3e984a6474951e2f65789ab2421d28
LJW25 <dbjwlee@gmail.com>
2024-01-20 18:56:13 +0900

    feat: 카테고리 수정 API 구현

--- backend/src/main/java/hanglog/admin/presentation/AdminCategoryController.java
+++ backend/src/main/java/hanglog/admin/presentation/AdminCategoryController.java
@@ -13,7 +13,9 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +44,16 @@ public class AdminCategoryController {
     ) {
         final Long categoryId = categoryService.save(categoryRequest);
         return ResponseEntity.created(URI.create("/admin/categories/" + categoryId)).build();
+    }
+
+    @PutMapping("/{categoryId}")
+    @AdminOnly
+    public ResponseEntity<Void> updateCategory(
+            @AdminAuth final Accessor accessor,
+            @PathVariable final Long categoryId,
+            @RequestBody @Valid final CategoryRequest categoryRequest
+    ) {
+        categoryService.update(categoryId, categoryRequest);
+        return ResponseEntity.noContent().build();
     }
 }
