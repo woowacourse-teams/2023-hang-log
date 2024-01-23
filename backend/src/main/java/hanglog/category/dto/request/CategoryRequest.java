d429a5a3a46937ee867096d0ee0d8bba84d1caeb
LJW25 <dbjwlee@gmail.com>
2024-01-23 23:22:10 +0900

    refactor: request 검증 메세지 수정

--- backend/src/main/java/hanglog/category/dto/request/CategoryRequest.java
+++ backend/src/main/java/hanglog/category/dto/request/CategoryRequest.java
@@ -17,7 +17,7 @@ public class CategoryRequest {
     @Size(max = 50, message = "영어이름은 50자를 초과할 수 없습니다.")
     private final String engName;
 
-    @NotBlank(message = "한글 이을 입력해주세요.")
+    @NotBlank(message = "한글 이름을 입력해주세요.")
     @Size(max = 50, message = "한글 이름은 50자를 초과할 수 없습니다.")
     private final String korName;
 }
--- backend/src/main/java/hanglog/currency/dto/request/CurrencyRequest.java
+++ backend/src/main/java/hanglog/currency/dto/request/CurrencyRequest.java
@@ -17,53 +17,53 @@ public class CurrencyRequest {
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private final LocalDate date;
 
-    @NotNull(message = "USD 금액을 입력해주세요.")
-    @DecimalMin(value = "0", message = "USD 금액이 0원보다 작을 수 없습니다.")
-    @DecimalMax(value = "100000", message = "USD 금액이 10만원보다 클 수 없습니다.")
+    @NotNull(message = "USD 환율을 입력해주세요.")
+    @DecimalMin(value = "0", message = "USD 환율이 0원보다 작을 수 없습니다.")
+    @DecimalMax(value = "100000", message = "USD 환율이 10만원보다 클 수 없습니다.")
     private final double usd;
 
-    @NotNull(message = "EUR 금액을 입력해주세요.")
-    @DecimalMin(value = "0", message = "EUR 금액이 0원보다 작을 수 없습니다.")
-    @DecimalMax(value = "100000", message = "EUR 금액이 10만원보다 클 수 없습니다.")
+    @NotNull(message = "EUR 환율을 입력해주세요.")
+    @DecimalMin(value = "0", message = "EUR 환율이 0원보다 작을 수 없습니다.")
+    @DecimalMax(value = "100000", message = "EUR 환율이 10만원보다 클 수 없습니다.")
     private final double eur;
 
-    @NotNull(message = "GBP 금액을 입력해주세요.")
-    @DecimalMin(value = "0", message = "GBP 금액이 0원보다 작을 수 없습니다.")
-    @DecimalMax(value = "100000", message = "GBP 금액이 10만원보다 클 수 없습니다.")
+    @NotNull(message = "GBP 환율을 입력해주세요.")
+    @DecimalMin(value = "0", message = "GBP 환율이 0원보다 작을 수 없습니다.")
+    @DecimalMax(value = "100000", message = "GBP 환율이 10만원보다 클 수 없습니다.")
     private final double gbp;
 
-    @NotNull(message = "JPY 금액을 입력해주세요.")
-    @DecimalMin(value = "0", message = "JPY 금액이 0원보다 작을 수 없습니다.")
-    @DecimalMax(value = "100000", message = "JPY 금액이 10만원보다 클 수 없습니다.")
+    @NotNull(message = "JPY 환율을 입력해주세요.")
+    @DecimalMin(value = "0", message = "JPY 환율이 0원보다 작을 수 없습니다.")
+    @DecimalMax(value = "100000", message = "JPY 환율이 10만원보다 클 수 없습니다.")
     private final double jpy;
 
-    @NotNull(message = "CNY 금액을 입력해주세요.")
-    @DecimalMin(value = "0", message = "CNY 금액이 0원보다 작을 수 없습니다.")
-    @DecimalMax(value = "100000", message = "CNY 금액이 10만원보다 클 수 없습니다.")
+    @NotNull(message = "CNY 환율을 입력해주세요.")
+    @DecimalMin(value = "0", message = "CNY 환율이 0원보다 작을 수 없습니다.")
+    @DecimalMax(value = "100000", message = "CNY 환율이 10만원보다 클 수 없습니다.")
     private final double cny;
 
-    @NotNull(message = "CHF 금액을 입력해주세요.")
-    @DecimalMin(value = "0", message = "CHF 금액이 0원보다 작을 수 없습니다.")
-    @DecimalMax(value = "100000", message = "CHF 금액이 10만원보다 클 수 없습니다.")
+    @NotNull(message = "CHF 환율을 입력해주세요.")
+    @DecimalMin(value = "0", message = "CHF 환율이 0원보다 작을 수 없습니다.")
+    @DecimalMax(value = "100000", message = "CHF 환율이 10만원보다 클 수 없습니다.")
     private final double chf;
 
-    @NotNull(message = "SGD 금액을 입력해주세요.")
-    @DecimalMin(value = "0", message = "SGD 금액이 0원보다 작을 수 없습니다.")
-    @DecimalMax(value = "100000", message = "SGD 금액이 10만원보다 클 수 없습니다.")
+    @NotNull(message = "SGD 환율을 입력해주세요.")
+    @DecimalMin(value = "0", message = "SGD 환율이 0원보다 작을 수 없습니다.")
+    @DecimalMax(value = "100000", message = "SGD 환율이 10만원보다 클 수 없습니다.")
     private final double sgd;
 
-    @NotNull(message = "THB 금액을 입력해주세요.")
-    @DecimalMin(value = "0", message = "THB 금액이 0원보다 작을 수 없습니다.")
-    @DecimalMax(value = "100000", message = "THB 금액이 10만원보다 클 수 없습니다.")
+    @NotNull(message = "THB 환율을 입력해주세요.")
+    @DecimalMin(value = "0", message = "THB 환율이 0원보다 작을 수 없습니다.")
+    @DecimalMax(value = "100000", message = "THB 환율이 10만원보다 클 수 없습니다.")
     private final double thb;
 
-    @NotNull(message = "HKD 금액을 입력해주세요.")
-    @DecimalMin(value = "0", message = "HKD 금액이 0원보다 작을 수 없습니다.")
-    @DecimalMax(value = "100000", message = "HKD 금액이 10만원보다 클 수 없습니다.")
+    @NotNull(message = "HKD 환율을 입력해주세요.")
+    @DecimalMin(value = "0", message = "HKD 환율이 0원보다 작을 수 없습니다.")
+    @DecimalMax(value = "100000", message = "HKD 환율이 10만원보다 클 수 없습니다.")
     private final double hkd;
 
-    @NotNull(message = "KRW 금액을 입력해주세요.")
-    @DecimalMin(value = "0", message = "KRW 금액이 0원보다 작을 수 없습니다.")
-    @DecimalMax(value = "100000", message = "KRW 금액이 10만원보다 클 수 없습니다.")
+    @NotNull(message = "KRW 환율을 입력해주세요.")
+    @DecimalMin(value = "0", message = "KRW 환율이 0원보다 작을 수 없습니다.")
+    @DecimalMax(value = "100000", message = "KRW 환율이 10만원보다 클 수 없습니다.")
     private final double krw;
 }
