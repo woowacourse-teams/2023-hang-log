568997699d9757c06aa53c0a527febee9e74c158
mcodnjs <codnjs8989@gmail.com>
2024-01-28 19:48:53 +0900

    feat: 좋아요 업데이트 로직 구현

--- backend/src/main/java/hanglog/like/service/LikeService.java
+++ backend/src/main/java/hanglog/like/service/LikeService.java
@@ -5,19 +5,22 @@ import static java.lang.Boolean.TRUE;
 import hanglog.like.domain.LikeCount;
 import hanglog.like.domain.Likes;
 import hanglog.like.domain.MemberLike;
-import hanglog.like.dto.TripLikeCount;
-import hanglog.like.dto.request.LikeRequest;
 import hanglog.like.domain.repository.CustomLikeRepository;
 import hanglog.like.domain.repository.LikeCountRepository;
 import hanglog.like.domain.repository.LikeRepository;
 import hanglog.like.domain.repository.MemberLikeRepository;
+import hanglog.like.dto.TripLikeCount;
+import hanglog.like.dto.request.LikeRequest;
 import hanglog.member.domain.repository.MemberRepository;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,11 +35,38 @@ public class LikeService {
     private final LikeCountRepository likeCountRepository;
     private final MemberRepository memberRepository;
     private final CustomLikeRepository customLikeRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     public void update(final Long memberId, final Long tripId, final LikeRequest likeRequest) {
-        updateMemberLikeCache(memberId, tripId, likeRequest);
-        updateLikeCountCache(tripId, likeRequest);
+        final SetOperations<String, Object> opsForSet = redisTemplate.opsForSet();
+        final String key = String.format("likes:%d", tripId);
+        if (TRUE.equals(opsForSet.isMember(key, memberId))) {
+            removeMemberInLike(key, memberId, likeRequest.getIsLike(), opsForSet);
+            return;
+        }
+        addMemberInLike(key, memberId, likeRequest.getIsLike(), opsForSet);
     }
+
+    private void removeMemberInLike(final String key, final Long memberId, final Boolean isLike,
+                                    final SetOperations<String, Object> opsForSet) {
+        if (!isLike) {
+            opsForSet.remove(key, memberId);
+            redisTemplate.expire(key, Duration.ofMinutes(90L));
+        }
+    }
+
+    private void addMemberInLike(final String key, final Long memberId, final Boolean isLike,
+                                 final SetOperations<String, Object> opsForSet) {
+        if (isLike) {
+            opsForSet.add(key, memberId);
+            redisTemplate.expire(key, Duration.ofMinutes(90L));
+        }
+    }
+
+//    public void update(final Long memberId, final Long tripId, final LikeRequest likeRequest) {
+//        updateMemberLikeCache(memberId, tripId, likeRequest);
+//        updateLikeCountCache(tripId, likeRequest);
+//    }
 
     private void updateMemberLikeCache(final Long memberId, final Long tripId, final LikeRequest likeRequest) {
         Map<Long, Boolean> tripLikeStatusMap = new HashMap<>();
--- backend/src/test/java/hanglog/integration/service/LikeServiceIntegrationTest.java
+++ backend/src/test/java/hanglog/integration/service/LikeServiceIntegrationTest.java
@@ -1,17 +1,19 @@
 package hanglog.integration.service;
 
 import static hanglog.integration.IntegrationFixture.TRIP_CREATE_REQUEST;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
-import hanglog.like.domain.LikeCount;
-import hanglog.like.domain.repository.LikeCountRepository;
-import hanglog.like.domain.repository.MemberLikeRepository;
 import hanglog.like.dto.request.LikeRequest;
 import hanglog.like.service.LikeService;
 import hanglog.trip.service.TripService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 
 class LikeServiceIntegrationTest extends RedisServiceIntegrationTest {
 
@@ -22,10 +24,14 @@ class LikeServiceIntegrationTest extends RedisServiceIntegrationTest {
     private LikeService likeService;
 
     @Autowired
-    private MemberLikeRepository memberLikeRepository;
+    private RedisTemplate<String, Object> redisTemplate;
 
-    @Autowired
-    private LikeCountRepository likeCountRepository;
+    private SetOperations<String, Object> opsForSet;
+
+    @BeforeEach
+    void setUp() {
+        opsForSet = redisTemplate.opsForSet();
+    }
 
     @DisplayName("해당 게시물의 좋아요 여부를 변경할 수 있다.")
     @Test
@@ -34,31 +40,59 @@ class LikeServiceIntegrationTest extends RedisServiceIntegrationTest {
         final LikeRequest likeTrueRequest = new LikeRequest(true);
         final LikeRequest likeFalseRequest = new LikeRequest(false);
 
-        final Long tripId = tripService.save(member.getId(), TRIP_CREATE_REQUEST);
+        final Long memberId = member.getId();
+        final Long tripId = tripService.save(memberId, TRIP_CREATE_REQUEST);
+        final String key = String.format("likes:%d", tripId);
 
         // when & then
         assertSoftly(softly -> {
-            softly.assertThat(memberLikeRepository.findById(member.getId())).isEmpty();
+            softly.assertThat(opsForSet.isMember(key, memberId)).isEqualTo(FALSE);
 
-            likeService.update(member.getId(), tripId, likeTrueRequest);
-            softly.assertThat(memberLikeRepository.findById(member.getId())).isPresent();
-            softly.assertThat(memberLikeRepository.findById(member.getId())
-                    .get()
-                    .getLikeStatusForTrip()
-                    .get(tripId)).isTrue();
-            softly.assertThat(likeCountRepository.findById(tripId)).isPresent();
-            softly.assertThat(likeCountRepository.findById(tripId)).get().usingRecursiveComparison()
-                    .isEqualTo(new LikeCount(tripId, 1L));
+            likeService.update(memberId, tripId, likeTrueRequest);
+            softly.assertThat(opsForSet.isMember(key, memberId)).isEqualTo(TRUE);
 
-            likeService.update(member.getId(), tripId, likeFalseRequest);
-            softly.assertThat(memberLikeRepository.findById(member.getId())).isPresent();
-            softly.assertThat(memberLikeRepository.findById(member.getId())
-                    .get()
-                    .getLikeStatusForTrip()
-                    .get(tripId)).isFalse();
-            softly.assertThat(likeCountRepository.findById(tripId)).isPresent();
-            softly.assertThat(likeCountRepository.findById(tripId)).get().usingRecursiveComparison()
-                    .isEqualTo(new LikeCount(tripId, 0L));
+            likeService.update(memberId, tripId, likeFalseRequest);
+            softly.assertThat(opsForSet.isMember(key, memberId)).isEqualTo(FALSE);
+        });
+    }
+
+    @DisplayName("이미 여행에 좋아요를 누른 유저가 다시 좋아요 추가 요청을 보내면 무시하고 값을 그대로 유지한다.")
+    @Test
+    void update_TrueRequestWhenIsMember() {
+        // given
+        final LikeRequest likeTrueRequest = new LikeRequest(true);
+
+        final Long memberId = member.getId();
+        final Long tripId = tripService.save(memberId, TRIP_CREATE_REQUEST);
+        final String key = String.format("likes:%d", tripId);
+
+        likeService.update(memberId, tripId, likeTrueRequest);
+
+        // when & then
+        assertSoftly(softly -> {
+            softly.assertThat(opsForSet.isMember(key, memberId)).isEqualTo(TRUE);
+            likeService.update(memberId, tripId, likeTrueRequest);
+            softly.assertThat(opsForSet.isMember(key, memberId)).isEqualTo(TRUE);
+        });
+    }
+
+    @DisplayName("여행에 좋아요를 누르지 않은 유저가 좋아요 삭제 요청을 보내면 무시하고 값을 그대로 유지한다.")
+    @Test
+    void update_FalseRequestWhenIsNotMember() {
+        // given
+        final LikeRequest likeFalseRequest = new LikeRequest(false);
+
+        final Long memberId = member.getId();
+        final Long tripId = tripService.save(memberId, TRIP_CREATE_REQUEST);
+        final String key = String.format("likes:%d", tripId);
+
+        likeService.update(memberId, tripId, likeFalseRequest);
+
+        // when & then
+        assertSoftly(softly -> {
+            softly.assertThat(opsForSet.isMember(key, memberId)).isEqualTo(FALSE);
+            likeService.update(memberId, tripId, likeFalseRequest);
+            softly.assertThat(opsForSet.isMember(key, memberId)).isEqualTo(FALSE);
         });
     }
 }
