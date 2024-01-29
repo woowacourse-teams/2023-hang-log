64dbb6fda70bdfbfdf72c1b583ca4575498ec0f6
mcodnjs <codnjs8989@gmail.com>
2024-01-29 20:44:11 +0900

    refactor: 사용하지 않는 dto 삭제

--- backend/src/main/java/hanglog/like/domain/repository/LikeRepository.java
+++ backend/src/main/java/hanglog/like/domain/repository/LikeRepository.java
@@ -2,7 +2,6 @@ package hanglog.like.domain.repository;
 
 import hanglog.like.domain.Likes;
 import hanglog.like.dto.LikeElement;
-import hanglog.like.dto.TripLikeCount;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -11,41 +10,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface LikeRepository extends JpaRepository<Likes, Long> {
-
-    @Query("""
-            SELECT new hanglog.like.dto.LikeElement
-            (l.tripId, COUNT(l.memberId), EXISTS(SELECT 1 FROM Likes l_1 WHERE l_1.memberId = :memberId AND l_1.tripId = l.tripId))
-            FROM Likes l
-            WHERE l.tripId in :tripIds
-            GROUP BY l.tripId
-             """)
-    List<LikeElement> findLikeCountAndIsLikeByTripIds(@Param("memberId") final Long memberId,
-                                                      @Param("tripIds") final List<Long> tripIds);
-
-    @Query("""
-            SELECT new hanglog.like.dto.LikeElement
-            (l.tripId, COUNT(l.memberId), EXISTS(SELECT 1 FROM Likes l_1 WHERE l_1.memberId = :memberId AND l_1.tripId = l.tripId))
-            FROM Likes l
-            WHERE l.tripId = :tripId
-            GROUP BY l.tripId
-             """)
-    Optional<LikeElement> findLikeCountAndIsLikeByTripId(@Param("memberId") final Long memberId,
-                                                         @Param("tripId") final Long tripId);
-
-    @Query("""
-            SELECT new hanglog.like.dto.TripLikeCount(l.tripId, COUNT(l.memberId))
-            FROM Likes l
-            GROUP BY l.tripId
-             """)
-    List<TripLikeCount> findCountByAllTrips();
-
-    @Query("""
-            SELECT new hanglog.like.dto.TripLikeCount(l.tripId, COUNT(l.memberId))
-            FROM Likes l
-            WHERE l.tripId = :tripId
-            GROUP BY l.tripId
-             """)
-    List<Long> findByTripId(@Param("tripId") final Long tripId);
 
     @Query(value = """
             SELECT l.trip_id AS tripId, COUNT(l.id) AS likeCount, GROUP_CONCAT(l.member_id) AS memberIds
