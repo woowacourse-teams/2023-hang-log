4583c0e62aa05a859577d9cc0c51e1a57d212565
mcodnjs <codnjs8989@gmail.com>
2024-01-30 03:02:30 +0900

    fix: like key prefix 수정

--- backend/src/main/java/hanglog/like/domain/LikeRedisConstants.java
+++ backend/src/main/java/hanglog/like/domain/LikeRedisConstants.java
@@ -4,7 +4,7 @@ import java.time.Duration;
 
 public class LikeRedisConstants {
 
-    public static final String LIKE_KEY_PREFIX = "like:";
+    public static final String LIKE_KEY_PREFIX = "like:*";
     public static final String KEY_SEPARATOR = ":";
     public static final String EMPTY_MARKER = "empty";
     public static final Duration LIKE_TTL = Duration.ofMinutes(90L);
