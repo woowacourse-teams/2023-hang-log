7c2e8ff711c5522550ac178b7c5a6ecb6dbcc902
mcodnjs <codnjs8989@gmail.com>
2024-02-01 01:16:37 +0900

    fix: 업데이트 시 캐시가 없는 경우 DB에서 조회해오도록 수정

--- backend/src/main/java/hanglog/like/service/LikeService.java
+++ backend/src/main/java/hanglog/like/service/LikeService.java
@@ -1,10 +1,15 @@
 package hanglog.like.service;
 
+import static hanglog.like.domain.LikeRedisConstants.EMPTY_MARKER;
 import static hanglog.like.domain.LikeRedisConstants.LIKE_TTL;
 import static hanglog.like.domain.LikeRedisConstants.generateLikeKey;
+import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 
+import hanglog.like.domain.repository.CustomLikeRepository;
+import hanglog.like.dto.LikeElement;
 import hanglog.like.dto.request.LikeRequest;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SetOperations;
@@ -16,16 +21,34 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class LikeService {
 
+    private final CustomLikeRepository customLikeRepository;
     private final RedisTemplate<String, Object> redisTemplate;
 
     public void update(final Long memberId, final Long tripId, final LikeRequest likeRequest) {
         final SetOperations<String, Object> opsForSet = redisTemplate.opsForSet();
         final String key = generateLikeKey(tripId);
+        if (FALSE.equals(redisTemplate.hasKey(key))) {
+            final LikeElement likeElement = customLikeRepository.findLikesElementByTripId(tripId)
+                    .orElse(LikeElement.empty(tripId));
+            cachingLike(likeElement);
+        }
+
         if (TRUE.equals(opsForSet.isMember(key, memberId))) {
             removeMemberInLike(key, memberId, likeRequest.getIsLike(), opsForSet);
             return;
         }
         addMemberInLike(key, memberId, likeRequest.getIsLike(), opsForSet);
+    }
+
+    private void cachingLike(final LikeElement likeElement) {
+        final SetOperations<String, Object> opsForSet = redisTemplate.opsForSet();
+        final String key = generateLikeKey(likeElement.getTripId());
+        opsForSet.add(key, EMPTY_MARKER);
+        final Set<Long> memberIds = likeElement.getMemberIds();
+        if (!memberIds.isEmpty()) {
+            opsForSet.add(key, likeElement.getMemberIds().toArray());
+        }
+        redisTemplate.expire(key, LIKE_TTL);
     }
 
     private void removeMemberInLike(final String key, final Long memberId, final Boolean isLike,
