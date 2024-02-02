a289ec872e9009ba4dc4cbd9934c6b6d371af362
LJW25 <dbjwlee@gmail.com>
2024-02-02 15:13:43 +0900

    refactor: 주석 제거

--- frontend-monorepo/packages/hanglog-admin/vite.config.ts
+++ frontend-monorepo/packages/hanglog-admin/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
   resolve: {
