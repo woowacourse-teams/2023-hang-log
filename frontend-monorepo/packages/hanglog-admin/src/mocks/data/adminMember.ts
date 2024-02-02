35a5af296cc591fcc3d303a125011d97f8da4483
LJW25 <dbjwlee@gmail.com>
2024-02-02 15:40:19 +0900

    feat: mock 데이터 추가

---
+++ frontend-monorepo/packages/hanglog-admin/src/mocks/data/adminMember.ts
@@ -0,0 +1,19 @@
+import type { AdminMemberData } from '@/types/adminMember';
+
+export const adminMembers: AdminMemberData[] = [
+  {
+    id: 1,
+    username: 'HangLog',
+    adminType: 'MASTER',
+  },
+  {
+    id: 2,
+    username: 'io25',
+    adminType: 'ADMIN',
+  },
+  {
+    id: 3,
+    username: 'raon',
+    adminType: 'ADMIN',
+  },
+];
