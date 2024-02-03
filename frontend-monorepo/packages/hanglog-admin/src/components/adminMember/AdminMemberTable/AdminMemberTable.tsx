bef31c0ff27bbebaf3707503813dc7b453bc91a7
LJW25 <dbjwlee@gmail.com>
2024-02-03 17:28:59 +0900

    feat: 관리자 멤버 조회 페이지 구현

---
+++ frontend-monorepo/packages/hanglog-admin/src/components/adminMember/AdminMemberTable/AdminMemberTable.tsx
@@ -0,0 +1,34 @@
+import type { AdminMemberData } from '@/types/adminMember';
+
+import { tableStyling } from './AdminMemberTable.style';
+
+interface AdminmemberTableProps {
+  adminMembers: AdminMemberData[];
+}
+
+const AdminMemberTable = ({ adminMembers }: AdminmemberTableProps) => {
+  return (
+    <table css={tableStyling}>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>계정명</th>
+          <th>등급</th>
+          <th> </th>
+        </tr>
+      </thead>
+      <tbody>
+        {adminMembers.map((adminMember) => (
+          <tr key={adminMember.id}>
+            <td>{adminMember.id}</td>
+            <td>{adminMember.username}</td>
+            <td>{adminMember.adminType}</td>
+            <td></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default AdminMemberTable;
--- frontend-monorepo/packages/hanglog-admin/src/pages/AdminMemberPage/AdminMemberPage.tsx
+++ frontend-monorepo/packages/hanglog-admin/src/pages/AdminMemberPage/AdminMemberPage.tsx
@@ -1,13 +1,67 @@
-import { Flex, Heading } from 'hang-log-design-system';
+import { useCallback, useEffect, useState } from 'react';
+
+import { Button, Flex, Heading, useOverlay } from 'hang-log-design-system';
 
 import SidebarNavigation from '@/components/common/SidebarNavigation/SidebarNavigation';
+import PageNavigation from '@/components/common/PageNavigation/PageNavigation';
+import AdminMemberTable from '@/components/adminMember/AdminMemberTable/AdminMemberTable';
+
+import { useAdminMemberQuery } from '@/hooks/api/useAdminMemberQuery';
+import { usePageIndex } from '@/hooks/common/usePageIndex';
+
+import { TABLE_ROW_LENGTH } from '@/constants/ui';
+
+import {
+  containerStyling,
+  titleStyling,
+  addButtonStyling,
+  tableStyling,
+} from './AdminMemberPage.style';
 
 const AdminMemberPage = () => {
+  const { isOpen: isAddModalOpen, open: openAddModal, close: closeAddModal } = useOverlay();
+
+  const [page, setPage] = useState<number>(1);
+
+  const { adminMemberData } = useAdminMemberQuery();
+
+  const lastPageIndex = Math.ceil(adminMemberData.length / TABLE_ROW_LENGTH);
+  const currentPageData = adminMemberData.slice(
+    (page - 1) * TABLE_ROW_LENGTH,
+    page * TABLE_ROW_LENGTH
+  );
+
+  const { pageIndexDatas, changeNavigationDatas } = usePageIndex(lastPageIndex, page);
+
+  const handleSetPage = useCallback((page: number) => {
+    setPage(page);
+  }, []);
+
+  useEffect(() => {
+    changeNavigationDatas();
+  }, [changeNavigationDatas, page]);
+
   return (
     <>
       <Flex>
         <SidebarNavigation />
-        <Heading>행록 관리자 멤버 관리 페이지입니다.</Heading>
+        <Flex styles={{ direction: 'column', align: 'center' }} css={containerStyling}>
+          <Heading size="large" css={titleStyling}>
+            관리자 멤버 관리
+          </Heading>
+          <Button variant="primary" css={addButtonStyling} onClick={openAddModal}>
+            추가하기
+          </Button>
+          <section css={tableStyling}>
+            <AdminMemberTable adminMembers={currentPageData} />
+          </section>
+          <PageNavigation
+            pages={pageIndexDatas}
+            selected={page}
+            maxPage={lastPageIndex}
+            onChangeNavigate={handleSetPage}
+          />
+        </Flex>
       </Flex>
     </>
   );
