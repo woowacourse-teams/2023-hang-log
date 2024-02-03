14377d94790e11520c0f1f80b3d1a7f473be0669
LJW25 <dbjwlee@gmail.com>
2024-02-04 01:02:43 +0900

    feat: 관리자멤버 스켈레톤 페이지 구현

--- frontend-monorepo/packages/hanglog-admin/src/router/AppRouter.tsx
+++ frontend-monorepo/packages/hanglog-admin/src/router/AppRouter.tsx
@@ -14,6 +14,7 @@ import CurrencyPageSkeleton from '@/pages/CurrencyPage/CurrencyPageSkeleton';
 import { PATH } from '@constants/path';
 import { Suspense } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import AdminMemberPageSkeleton from '@/pages/AdminMemberPage/AdminMemberPageSkeleton';
 
 const router = createBrowserRouter([
   {
@@ -36,7 +37,7 @@ const router = createBrowserRouter([
       {
         path: PATH.ADMIN_MEMBER,
         element: (
-          <Suspense>
+          <Suspense fallback={<AdminMemberPageSkeleton />}>
             <AdminMemberPage />
           </Suspense>
         ),
